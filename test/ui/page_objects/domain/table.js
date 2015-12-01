ebca1e817e5294533f5783ed4f1d9a19362d3341
mhca <heimy.cruz@gmail.com>
2015-12-01 01:08:32 -0400

    Adding Page Objects for Domains' list and table.

---
+++ test/ui/page_objects/domain/table.js
@@ -0,0 +1,237 @@
+var tableHeaderLocators = {
+  name: {
+    css: 'th:nth-of-type(1) a'
+  },
+  cName: {
+    css: 'th:nth-of-type(2) a'
+  },
+  lastUpdated: {
+    css: 'th:nth-of-type(3)'
+  },
+  stagingGlobalStatus: {
+    css: 'th:nth-of-type(4)'
+  }
+};
+
+var tableRowLocators = {
+  name: {
+    css: 'td:nth-of-type(1)'
+  },
+  cName: {
+    css: 'td:nth-of-type(2)'
+  },
+  lastUpdated: {
+    css: 'td:nth-of-type(3)'
+  },
+  status: {
+    css: 'td:nth-of-type(4)',
+    icons: {
+      staging:{
+        css: 'i:nth-of-type(1)',
+        type: {
+          published:{
+            css: 'i:nth-of-type(1).glyphicon-ok-circle'
+          },
+          error:{
+            css: 'i:nth-of-type(1).glyphicon-remove'
+          }
+        }
+      },
+      global:{
+        css: 'i:nth-of-type(2)',
+        type: {
+          published:{
+            css: 'i:nth-of-type(2).glyphicon-ok-circle'
+          },
+          error:{
+            css: 'i:nth-of-type(2).glyphicon-remove'
+          }
+        }
+      }
+
+    }
+  },
+  actions: {
+    css: 'td:nth-of-type(5)',
+    buttons: {
+      pencil: {
+        className: 'glyphicon-pencil'
+      },
+      cog: {
+        className: 'glyphicon-cog'
+      },
+      trash: {
+        className: 'glyphicon-trash'
+      },
+      stats: {
+        className: 'glyphicon-stats'
+      },
+      book: {
+        className: 'glyphicon-book'
+      }
+    }
+  }
+};
+
+var TableRow = function (rowEl, locators) {
+
+  // Properties
+  this.rowEl = rowEl;
+  this.locators = locators;
+
+  // Methods
+  this.getNameCell = function () {
+    return this.rowEl.element(by.css(this.locators.name.css));
+  };
+
+  this.getCNameCell = function () {
+    return this.rowEl.element(by.css(this.locators.cName.css));
+  };
+
+  this.getLastUpdatedCell = function () {
+    return this.rowEl.element(by.css(this.locators.lastUpdated.css));
+  };
+
+  this.getStagingStatusCell = function () {
+    return this.rowEl.element(by.css(this.locators.status.icons.staging.css));
+  };
+
+  this.getGlobalStatusCell = function () {
+    return this.rowEl.element(by.css(this.locators.status.icons.global.css));
+  };
+
+  this.getName = function () {
+    return this
+      .getNameCell()
+      .getText();
+  };
+
+  this.getCName = function () {
+    return this
+      .getCNameCell()
+      .getText();
+  };
+
+  this.getLastUpdated = function () {
+    return this
+      .getLastUpdatedCell()
+      .getText();
+  };
+
+
+
+  if (this.locators.actions && this.locators.actions.buttons.pencil) {
+
+    this.getEditBtn = function () {
+      return this.rowEl
+        .element(by.css(this.locators.actions.css))
+        .element(by.className(this.locators.actions.buttons.pencil.className));
+    };
+
+    this.clickEdit = function () {
+      return this
+        .getEditBtn()
+        .click();
+    };
+  }
+
+  if (this.locators.actions && this.locators.actions.buttons.cog) {
+
+    this.getSettingsBtn = function () {
+      return this.rowEl
+        .element(by.css(this.locators.actions.css))
+        .element(by.className(this.locators.actions.buttons.cog.className));
+    };
+
+    this.clickSettings = function () {
+      return this
+        .getSettingsBtn()
+        .click();
+    };
+  }
+
+  if (this.locators.actions && this.locators.actions.buttons.trash) {
+
+    this.getDeleteBtn = function () {
+      return this.rowEl
+        .element(by.css(this.locators.actions.css))
+        .element(by.className(this.locators.actions.buttons.trash.className));
+    };
+
+    this.clickDelete = function () {
+      return this
+        .getDeleteBtn()
+        .click();
+    };
+  }
+
+
+  if (this.locators.actions && this.locators.actions.buttons.stats) {
+
+    this.getStatisticsBtn = function () {
+      return this.rowEl
+        .element(by.css(this.locators.actions.css))
+        .element(by.className(this.locators.actions.buttons.stats.className));
+    };
+
+    this.clickStatistics = function () {
+      return this
+        .getStatisticsBtn()
+        .click();
+    };
+  }
+
+  if (this.locators.actions && this.locators.actions.buttons.book) {
+
+    this.getHistoricalBtn = function () {
+      return this.rowEl
+        .element(by.css(this.locators.actions.css))
+        .element(by.className(this.locators.actions.buttons.book.className));
+    };
+
+    this.clickStatistics = function () {
+      return this
+        .getHistoricalBtn()
+        .click();
+    };
+  }
+
+};
+
+var DomainTable = {
+
+  locators: {
+    header: {
+      css: 'table thead tr'
+    },
+    rows: {
+      repeater: 'item in records'
+    }
+  },
+
+  getHeaderEl: function () {
+    return element(by.css(this.locators.header.css));
+  },
+
+  getHeader: function () {
+    var header = this.getHeaderEl();
+    return new TableRow(header, tableHeaderLocators);
+  },
+
+  getRows: function () {
+    return element.all(by.repeater(this.locators.rows.repeater));
+  },
+
+  getFirstRow: function () {
+    return this.getRow(0);
+  },
+
+  getRow: function (rowIndex) {
+    var el = this
+      .getRows()
+      .get(rowIndex);
+    return new TableRow(el, tableRowLocators);
+  }
+};
+
+module.exports = DomainTable;
