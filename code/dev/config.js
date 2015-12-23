fe19886e6e009fbd1b05132adf17e52b97026cb8
DmitryM <dmitry@mainlywrenches.co>
2015-12-23 08:37:50 +0400

    config corrected

--- code/dev/config.js
+++ code/dev/config.js
@@ -1,7 +1,7 @@
-// window.API_URL = 'https://testsjc20-api01.revsw.net/v1';
+window.API_URL = 'https://testsjc20-api01.revsw.net/v1';
 
 //  local debug
 // window.API_URL = 'https://localhost:8000/v1';
 
-window.API_URL = 'https://iad02-api03.revsw.net/v1';
+// window.API_URL = 'https://iad02-api03.revsw.net/v1';
 // window.API_URL = 'https://api.revsw.net/v1';
