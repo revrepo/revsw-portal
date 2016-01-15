d4f81fa12c920fdad471e47bab78f5e615ac15f6
DmitryM <dmitry@mainlywrenches.co>
2016-01-16 02:09:55 +0400

    mobile apps list linked to mobile analytics, app selected

--- code/dev/js/portal/reports/mobile.routes.js
+++ code/dev/js/portal/reports/mobile.routes.js
@@ -34,60 +34,6 @@
             templateUrl: 'parts/reports/mobile-distributions.html'
           }
         }
-      })
-      // .state('index.reports.topreports', {
-      //   url: '/reports/topreports',
-      //   views: {
-      //     main: {
-      //       controller: 'TopReportsTrafficController',
-      //       templateUrl: 'parts/reports/top-reports.html'
-      //     }
-      //   }
-      // })
-      // .state('index.reports.top', {
-      //   url: '/reports/top',
-      //   views: {
-      //     main: {
-      //       controller: 'TopObjectsController',
-      //       templateUrl: 'parts/reports/top.html'
-      //     }
-      //   }
-      // })
-      // .state('index.reports.fbt', {
-      //   url: '/reports/fbt',
-      //   views: {
-      //     main: {
-      //       controller: 'FBTReportsController',
-      //       templateUrl: 'parts/reports/fbt.html'
-      //     }
-      //   }
-      // })
-      // .state('index.reports.mob', {
-      //   url: '/reports/mobile',
-      //   views: {
-      //     main: {
-      //       controller: 'MobileReportsController',
-      //       templateUrl: 'parts/reports/mobile.html'
-      //     }
-      //   }
-      // })
-      // .state('index.reports.heatmaps', {
-      //   url: '/reports/heatmaps',
-      //   views: {
-      //     main: {
-      //       controller: 'TrafficHeatmapsController',
-      //       templateUrl: 'parts/reports/heatmaps.html'
-      //     }
-      //   }
-      // })
-      // .state('index.reports.lm_rtt_heatmaps', {
-      //   url: '/reports/lm_rtt_heatmaps',
-      //   views: {
-      //     main: {
-      //       controller: 'LMRTTHeatmapsController',
-      //       templateUrl: 'parts/reports/lm-rtt-heatmaps.html'
-      //     }
-      //   }
-      // });
+      });
   }
 })();
