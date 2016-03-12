5ca729d1d8d752dd7121bc974ca6b316cd7443ba
Stanislav Sorokin <stanislav.sorokin@linkinlive.com>
2016-03-12 15:36:39 +0200

    reportsFilterServc, general view, ctrl

--- code/dev/js/portal/reports/controller/general.controller.js
+++ code/dev/js/portal/reports/controller/general.controller.js
@@ -15,18 +15,21 @@
 
   GeneralCtrl.$inject = [
     '$scope',
-    'Stats'
+    'Stats',
+    'reportsFilterService'
   ];
 
   /*@ngInject*/
   function GeneralCtrl(
     $scope,
-    Stats
+    Stats,
+    reportsFilterService
   ) {
     var vm = this;
 
     //ui data model
     vm.model = {
+      total: {},
       domain: empty,
       filtersList: []
     };
@@ -46,6 +49,7 @@
      * @kind function
      */
     function init() {
+      //{domainId: "568525ec6f641ea7285c4221", from_timestamp: 1457697412757, to_timestamp: 1457783812757}
     }
 
     /**
@@ -56,55 +60,95 @@
     function onDomainChange() {
       if (vm.model.domain) {
         var domainId = vm.model.domain.id;
-        reloadOS(domainId);
-        reloadDevice(domainId);
+        getFilterData(domainId);
       }
-      console.log(vm.model.domain);
-      //
-      //reloadDevice();
     }
 
     /**
-     * Reload list of OS
-     *
-     * @param {string|number} domainId
+     * @name getFilterData
+     * @desc get Filter data
+     * @kind function
      */
-    function reloadOS(domainId) {
-      $scope.os = {
-        labels: [],
-        data: []
-      };
+    function getFilterData(domainId) {
+      reportsFilterService
+        .getOs(domainId)
+        .then(function(osData) {
+          $scope.os = osData;
+        });
 
-      Stats.os({ domainId: domainId }).$promise.then(function(data) {
-        if (data.data && data.data.length > 0) {
-          angular.forEach(data.data, function(os) {
-            $scope.os.labels.push(os.key);
-            $scope.os.data.push(os.count);
-          });
-        }
-      });
+      reportsFilterService
+        .getDevices(domainId)
+        .then(function(deviceData) {
+          $scope.device = deviceData;
+        });
+
+      reloadCacheStatus();
+      reloadHttpMethod();
+      getTotal();
     }
 
     /**
-     * List of devices
-     *
-     * @param {string|number} domainId
+     * @name reloadCacheStatus
+     * @desc reloads cache status chart data
+     * @kind function
      */
-    function reloadDevice(domainId) {
-      $scope.device = {
-        labels: [],
-        data: []
-      };
+    function reloadCacheStatus(filters) {
+      filters = filters || { domainId: '568525ec6f641ea7285c4221', from_timestamp: 1457697412757, to_timestamp: 1457783812757 };
+      $scope.cacheStatus = [];
+      Stats.cacheStatus(filters)
+        .$promise
+        .then(function(data) {
+          if (data.data && data.data.length > 0) {
+            var newData = [];
+            _.forEach(data.data, function(val) {
+              newData.push({
+                name: val.key,
+                y: val.count
+              });
+            });
+            $scope.cacheStatus = newData;
+          }
+        });
+    }
 
-      Stats.device({ domainId: domainId }).$promise.then(function(data) {
-        if (data.data && data.data.length > 0) {
-          angular.forEach(data.data, function(os) {
-            $scope.device.labels.push(os.key);
-            $scope.device.data.push(os.count);
-            console.log($scope.device);
-          });
-        }
-      });
+    /**
+     * @name reloadHttpMethod
+     * @desc reloads data for the http method chart
+     * @kind function
+     */
+    function reloadHttpMethod(filters) {
+      filters = filters || { domainId: '568525ec6f641ea7285c4221', from_timestamp: 1457697412757, to_timestamp: 1457783812757 };
+      $scope.httpMethod = [];
+      Stats.httpMethod( filters )
+        .$promise
+        .then(function (data) {
+          if (data.data && data.data.length > 0) {
+            var newData = [];
+            _.forEach(data.data, function (val) {
+              newData.push({
+                name: val.key,
+                y: val.count
+              });
+            });
+            $scope.httpMethod = newData;
+          }
+        });
+    }
+
+    /**
+     * @name getTotal
+     * @desc gets data for the total
+     * @kind function
+     */
+    function getTotal() {
+      vm.model.total = {
+        traficThisMonth: 123,
+        cacheHitRate: 68,
+        failedRequests: 0.01,
+        ajaxHttpResponses: 0.01,
+        averageResponseTime: 1243,
+        averageTTFBTime: 45
+      };
     }
   }
 })(angular);
