cb37cd93cdc72b8005767bef2f1737049cdd8fca
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-03-14 13:00:22 +0700

    update widget "adf-widget-analytics-proxy-traffic"
    - create new templates
    - add new types widget content

--- code/widgets/adf-widget-analytics-proxy-traffic/dist/adf-widget-analytics-proxy-traffic.js
+++ code/widgets/adf-widget-analytics-proxy-traffic/dist/adf-widget-analytics-proxy-traffic.js
@@ -21,44 +21,42 @@ angular.module('adf.widget.analytics-proxy-traffic', ['adf.provider'])
       .widget('analytics-proxy-traffic-bandwidth-usage', angular.extend(_widget, {
         title: 'Bandwidth Usage',
         description: 'Display the Bandwidth Usage (requests-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' requests-chart' +
-          ' status-codes="config.statusCode.labels"' +
-          ' fl-os="config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">Bandwidth Usage</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-requests-chart.tpl.html',
       }))
       .widget('analytics-proxy-traffic', angular.extend(_widget, {
         title: 'Total Requests',
         description: 'Display the Total Requests (proxy-traffic-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' proxy-traffic-chart' +
-          ' status-codes="config.statusCode.labels"' +
-          ' fl-os="config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">Proxy Traffic</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-proxy-traffic-chart.tpl.html',
       }))
       .widget('analytics-proxy-traffic-http-https-chart', angular.extend(_widget, {
         title: 'HTTP/HTTPS Hits',
         description: 'Display the HTTP/HTTPS Hits (http-https-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' http-https-chart' +
-          ' fl-os="config.config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">HTTP/HTTPS Hits</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-http-https-chart.tpl.html',
       }))
+      .widget('analytics-proxy-http-status-code-chart', angular.extend(_widget, {
+        title: 'HTTP Status Code Hits',
+        description: 'Display the HTTP Status Code Hits (http-status-code-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-http-status-code-chart.tpl.html',
+      }))
+      .widget('analytics-proxy-request-status-chart', angular.extend(_widget, {
+        title: 'Success/Failure Request Status',
+        description: 'Display the Success/Failure Request Status(request-status-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-request-status-chart.tpl.html',
+      }))
+      .widget('analytics-proxy-hits-cache-chart', angular.extend(_widget, {
+        title: 'Edge Cache Efficiency Hits',
+        description: 'Display the Edge Cache Efficiency Hits(hits-cache-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-hits-cache-chart.tpl.html',
+      }));
 
-    ;
   }]);
 
-angular.module("adf.widget.analytics-proxy-traffic").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/edit.html","<form role=form><div class=form-group><label for=sample>Sample</label> <input type=text class=form-control id=sample ng-model=config.sample placeholder=\"Enter sample\"></div><div class=form-group><label for=domain>domain</label><div domain-select id=domain ng-model=config.domain on-select=onDomainSelected() select-one=true></div></div></form>");
-$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/view.html","<div><h1>Widget view</h1><p>Content of {{config.sample}}</p></div>");}]);})(window);
+angular.module("adf.widget.analytics-proxy-traffic").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/edit.html","<form role=form><div class=form-group><label for=domain>domain</label><div domain-select id=domain ng-model=config.domain on-select=onDomainSelected() select-one=true></div></div><div class=form-group><label for=domain>domain</label><div domain-select id=domain ng-model=config.domain on-select=onDomainSelected() select-one=true></div></div></form>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/title.tpl.html","<h1>Hello</h1>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/view.html","<div><h1>Widget view</h1><p>Content of {{config.sample}}</p></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-hits-cache-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div hits-cache-chart fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-http-https-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div http-https-chart fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-http-status-code-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div http-status-code-chart status-codes=config.statusCode.labels fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-proxy-traffic-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div proxy-traffic-chart status-codes=statusCode.labels fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-request-status-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div request-status-chart status-codes=config.statusCode.labels fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");
+$templateCache.put("{widgetsPath}/analytics-proxy-traffic/src/views/view-requests-chart.tpl.html","<div class=col-lg-12><h4>{{config.domain.domain_name}}</h4><div requests-chart fl-os=config.os.labels fl-device=config.device.labels fl-country=config.countries ng-domain=config.domain></div></div>");}]);})(window);
--- code/widgets/adf-widget-analytics-proxy-traffic/src/analytics-proxy-traffic.js
+++ code/widgets/adf-widget-analytics-proxy-traffic/src/analytics-proxy-traffic.js
@@ -20,41 +20,32 @@ angular.module('adf.widget.analytics-proxy-traffic', ['adf.provider'])
       .widget('analytics-proxy-traffic-bandwidth-usage', angular.extend(_widget, {
         title: 'Bandwidth Usage',
         description: 'Display the Bandwidth Usage (requests-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' requests-chart' +
-          ' status-codes="config.statusCode.labels"' +
-          ' fl-os="config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">Bandwidth Usage</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-requests-chart.tpl.html',
       }))
       .widget('analytics-proxy-traffic', angular.extend(_widget, {
         title: 'Total Requests',
         description: 'Display the Total Requests (proxy-traffic-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' proxy-traffic-chart' +
-          ' status-codes="config.statusCode.labels"' +
-          ' fl-os="config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">Proxy Traffic</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-proxy-traffic-chart.tpl.html',
       }))
       .widget('analytics-proxy-traffic-http-https-chart', angular.extend(_widget, {
         title: 'HTTP/HTTPS Hits',
         description: 'Display the HTTP/HTTPS Hits (http-https-chart)',
-        template: '<div class="col-lg-12">' +
-          '<div ' +
-          ' http-https-chart' +
-          ' fl-os="config.config.os.labels"' +
-          ' fl-device="config.device.labels"' +
-          ' fl-country="config.countries"' +
-          ' ng-domain="config.domain">HTTP/HTTPS Hits</div>' +
-          '</div>',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-http-https-chart.tpl.html',
       }))
+      .widget('analytics-proxy-http-status-code-chart', angular.extend(_widget, {
+        title: 'HTTP Status Code Hits',
+        description: 'Display the HTTP Status Code Hits (http-status-code-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-http-status-code-chart.tpl.html',
+      }))
+      .widget('analytics-proxy-request-status-chart', angular.extend(_widget, {
+        title: 'Success/Failure Request Status',
+        description: 'Display the Success/Failure Request Status(request-status-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-request-status-chart.tpl.html',
+      }))
+      .widget('analytics-proxy-hits-cache-chart', angular.extend(_widget, {
+        title: 'Edge Cache Efficiency Hits',
+        description: 'Display the Edge Cache Efficiency Hits(hits-cache-chart)',
+        templateUrl: '{widgetsPath}/analytics-proxy-traffic/src/views/view-hits-cache-chart.tpl.html',
+      }));
 
-    ;
   });
