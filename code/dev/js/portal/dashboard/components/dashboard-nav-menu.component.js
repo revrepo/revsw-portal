cf53aa71cafdab8f72acda206297ec51df1fb2e3
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-03-17 14:14:10 +0700

    Open default dashboard

--- code/dev/js/portal/dashboard/components/dashboard-nav-menu.component.js
+++ code/dev/js/portal/dashboard/components/dashboard-nav-menu.component.js
@@ -13,7 +13,7 @@
       restrict: 'AE',
       replace: true,
       template: '  <li ui-sref-active-if="{class: \'active-side-menu-item\', state: \'index.dashboard\'}">' +
-        '  <a ui-sref="index.dashboard" class="side-menu-item">Dashboards</a>' +
+        '  <a ui-sref="index.dashboard.details({dashboardId:vm.dashboardsList[0].id})" class="side-menu-item">Dashboards</a>' +
         '  <a ui-sref-active="active" ui-sref="index.dashboard.details({dashboardId:dash.id})" ng-repeat="dash in vm.dashboardsList" class="side-menu-sub-item">{{dash.title}}</a>' +
         '</li>',
       scope: false,
