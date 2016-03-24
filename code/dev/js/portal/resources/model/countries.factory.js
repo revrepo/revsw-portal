0ca84ce15b10ef7037fe4e9672db23177c012b06
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-03-24 14:38:21 +0700

    Change Countries factory for caching request results

--- code/dev/js/portal/resources/model/countries.factory.js
+++ code/dev/js/portal/resources/model/countries.factory.js
@@ -1,16 +1,45 @@
-(function () {
+(function() {
   'use strict';
 
   angular
     .module('revapm.Portal.Resources')
-    .factory('Countries', CountriesResource);
+    .factory('Countries', CountriesService)
+    .factory('CountriesResource', CountriesResource);
+
 
   /*@ngInject*/
   function CountriesResource(Resource, $config) {
-
     return Resource($config.API_URL + '/countries/list', {}, {
-      query: {isArray: false}
+      query: {
+        isArray: false
+      }
     });
-
+  }
+  /*@ngInject*/
+  function CountriesService($q, $http, $config) {
+    // NOTE: query method used attribute "countryList" for caching data
+    return {
+      query: function() {
+        if (!this.countryList) {
+          this.countryList = {};
+        }
+        var deffer = $q.defer();
+        if (_.isEmpty(this.countryList)) {
+          var scope = this;
+          $http.get($config.API_URL + '/countries/list')
+            .then(function(data) {
+                scope.countryList = data.data;
+                deffer.resolve(data.data);
+                return data.data;
+              },
+              function(err) {
+                deffer.reject(err);
+              });
+        } else {
+          deffer.resolve(this.countryList);
+        }
+        return deffer.promise;
+      }
+    };
   }
 })();
