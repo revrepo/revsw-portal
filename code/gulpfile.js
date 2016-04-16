dd6525bf48acecea4c5c2be6d7a29533fc8327fb
Victor Gartvich <victor@revsw.com>
2016-04-16 06:12:10 +0000

    Fixing heatmap JS files

--- code/gulpfile.js
+++ code/gulpfile.js
@@ -92,8 +92,7 @@ var stylish = require('jshint-stylish');
 gulp.task('lintjs', function () {
   return gulp.src([
       'gulpfile.js',
-      devFolder + 'js/**/*.js',
-      '!' + devFolder + 'js/heatmaps/*.js'])
+      devFolder + 'js/**/*.js'])
     .pipe(jshint({linter: 'jshint'}))
     .pipe(jshint.reporter(stylish));
 });
