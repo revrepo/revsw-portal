d6f23a829d79ad5f6f003c2398f0b36b7b64f72f
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-05-05 02:15:43 +0700

    Save changes for simplify singup process

--- code/dev/js/portal/signup/controller/verify.controller.js
+++ code/dev/js/portal/signup/controller/verify.controller.js
@@ -26,24 +26,6 @@
         })
         .catch(AlertService.danger);
     };
-    // TODO: delete - depricated
-    // $scope.getQueryString = function(model) {
-    //   var q = '?first_name=' + encodeURIComponent(model.firstname ? model.firstname : '') +
-    //     '&last_name=' + encodeURIComponent(model.lastname ? model.lastname : '') +
-    //     '&email=' + encodeURIComponent(model.email ? model.email : '') +
-    //     '&phone=' + encodeURIComponent(model.phone_number ? model.phone_number : '') +
-    //     '&reference=' + encodeURIComponent(model.user_id ? model.user_id : '') +
-    //     '&organization=' + encodeURIComponent(model.companyName ? model.companyName : '') +
-    //     '&billing_address=' + encodeURIComponent(model.address1 ? model.address1 : '') +
-    //     '&billing_address_2=' + encodeURIComponent(model.address2 ? model.address2 : '') +
-    //     '&billing_city=' + encodeURIComponent(model.city ? model.city : '') +
-    //     '&billing_state=' + encodeURIComponent((model.state && model.country === 'US') ?
-    //       model.state : '') +
-    //     '&billing_zip=' + encodeURIComponent(model.zipcode ? model.zipcode : '') +
-    //     '&billing_country=' + encodeURIComponent(model.country ? model.country : '');
-    //   return q;
-    // };
-
 
     if ($stateParams.token) {
       Users.verify({
@@ -54,20 +36,19 @@
           $localStorage.user = {
             email: res.email
           };
-          // TODO: delete -deprecated
-          // var q = $scope.getQueryString(res);
-          // $window.location.href = res.hosted_page + q;
-          // TODO: applay auto login !!!!
-          // console.log(res) ;
           // TODO: inform into modal window
-          $state.go('index');
+          return User.updateToken(res.token)
+            .then(User.reloadUser)
+            .then(function() {
+              $state.go('index');
+            }, function(err) {
+              AlertService.danger(err.message);
+            });
         }, function(err) {
           //TODO: change message - show message from server
-          //console.log('Verify error',err);
           AlertService.danger('Oops something went wrong', 5000);
         })
         .catch(function(err) {
-          AlertService.danger(err.data.message);
           // TODO: detect type error
           //$state.go('resend_token');
         });
