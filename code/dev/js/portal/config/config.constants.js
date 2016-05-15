03a38c77f3d98513c1b162a6d08fab76a99eb209
Victor Gartvich <victor@revsw.com>
2016-05-15 21:56:58 +0000

    Temporary disabling IntroJS

--- code/dev/js/portal/config/config.constants.js
+++ code/dev/js/portal/config/config.constants.js
@@ -137,7 +137,7 @@
        * Interval delay for refreshing Log Shippers status
        */
       LOGSHIPPERS_STATUS_REFRESH_INTERVAL: 25000,
-      INTRO_IS_ACTIVE: true,
+      INTRO_IS_ACTIVE: false,
       ENABLE_SIMPLIFIED_SIGNUP_PROCESS: true,
 
       // Used by Angular Toaster
