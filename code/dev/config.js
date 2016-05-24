6cb69bdb504b1e474bd9c7b36b393037be659d56
Victor Gartvich <victor@revsw.com>
2016-05-24 19:35:45 +0000

    Fixed workflow tests for users

--- code/dev/config.js
+++ code/dev/config.js
@@ -14,4 +14,4 @@ window.API_URL = 'https://testsjc20-api01.revsw.net/v1';
 // Google Analytic Account ID
 window.gaAccount = 'UA-68856309-2'; // develop
 // Intro configuration flag (true=on, false=off)
-window.intro = false;
+// window.intro = false;
