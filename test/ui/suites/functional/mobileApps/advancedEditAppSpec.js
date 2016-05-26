4ae61cbff5e12ff552bffcfa12eae5ace51b6a96
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-05-26 23:56:12 +0700

    Fix jshint error for test/ui/suites/functional/mobileApps/advancedEditAppSpec.js

--- test/ui/suites/functional/mobileApps/advancedEditAppSpec.js
+++ test/ui/suites/functional/mobileApps/advancedEditAppSpec.js
@@ -27,9 +27,9 @@ describe('Functional', function () {
     var adminUser = config.get('portal.users.admin');
     var iosApps = DataProvider.generateMobileAppData('iOS', 1);
     var androidApps = DataProvider.generateMobileAppData('Android', 1);
-    var windowsMobileApps = DataProvider.generateMobileAppData('Windows_Mobile', 1);
+    // var windowsMobileApps = DataProvider.generateMobileAppData('Windows_Mobile', 1);
     var apps = iosApps.concat(androidApps);//.concat(windowsMobileApps);
-    console.log(apps)
+
     beforeAll(function () {
       Portal.signIn(adminUser);
       Portal.createMobileApps('iOS', iosApps);
