573348b0ffbb4b27d4fe118691607cea779bf52e
Victor Gartvich <victor@revsw.com>
2016-05-30 17:52:29 +0000

    [ci skip] Fixing app tests

--- test/ui/page_objects/mobileApp/editForm.js
+++ test/ui/page_objects/mobileApp/editForm.js
@@ -774,9 +774,9 @@ var EditAppForm = {
     this.setConfigurationStaleTimeout(app.configurationStaleTimeout);
     this.setInitialTransportProtocol(app.initialTransportProtocol);
     this.setAnalyticsReportingLevel(app.analyticsReportingLevel);
-    this.setDomainsWhiteList(app.domainsWhiteList);
-    this.setDomainsBlackList(app.domainsBlackList);
-    this.setDomainsProvisionedList(app.domainsProvisionedList);
+//    this.setDomainsWhiteList(app.domainsWhiteList);
+//    this.setDomainsBlackList(app.domainsBlackList);
+//    this.setDomainsProvisionedList(app.domainsProvisionedList);
     this.setTestingOffloadingRatio(app.testingOffloadingRatio);
     this.setComment(app.comment);
   }
