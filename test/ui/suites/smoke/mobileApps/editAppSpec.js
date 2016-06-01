719078bfca2de705a7a76329c8fccc02d0da47ac
Vadym Morozov <vadym.morozov89@gmail.com>
2016-06-01 12:38:09 +0300

    fixed method for QUIC

--- test/ui/suites/smoke/mobileApps/editAppSpec.js
+++ test/ui/suites/smoke/mobileApps/editAppSpec.js
@@ -120,9 +120,6 @@ describe('Smoke', function () {
                 }
                 if(app.allowedTransportProtocolsAndSelectionPriority==='QUIC')
                 {
-/*                    Portal.mobileApps.editPage.form.getAllowedTransportProtocolsAndSelectionPriorityQUIC().getAttribute('checked').then(function(value){
-                     expect(value).toEqual('true');
-                    });*/
                     Portal.mobileApps.editPage.form.getAllowedTransportProtocolsAndSelectionPriorityQUIC().getAttribute('checked').then(function(value){
                      expect(value).toEqual('true');
                     });
