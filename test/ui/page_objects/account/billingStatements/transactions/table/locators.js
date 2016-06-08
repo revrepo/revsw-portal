b267f145adbc61cae3d0931a8521b3d355b7cd29
Leo <leo.fcx@gmail.com>
2016-06-08 00:27:59 -0400

    Adding locators for tables

--- test/ui/page_objects/account/billingStatements/transactions/table/locators.js
+++ test/ui/page_objects/account/billingStatements/transactions/table/locators.js
@@ -1,3 +1,62 @@
-/**
- * Created by leo on 06-06-16.
+/*************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2016] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
  */
+
+var tableLocators = {
+  header: {
+    css: 'table.dataTable:nth-of-type(1) thead tr'
+  },
+  rows: {
+    repeater: 'transaction in transactions'
+  }
+};
+
+var headerLocators = {
+  operationType: {
+    css: 'table.dataTable:nth-of-type(1) th:nth-of-type(1) a'
+  },
+  date: {
+    css: 'table.dataTable:nth-of-type(1) th:nth-of-type(2) a'
+  },
+  memo: {
+    css: 'table.dataTable:nth-of-type(1) th:nth-of-type(3) a'
+  },
+  amount: {
+    css: 'table.dataTable:nth-of-type(1) th:nth-of-type(4) a'
+  }
+};
+
+var rowLocators = {
+  operationType: {
+    css: 'table.dataTable:nth-of-type(1) td:nth-of-type(1)'
+  },
+  date: {
+    css: 'table.dataTable:nth-of-type(1) td:nth-of-type(2)'
+  },
+  memo: {
+    css: 'table.dataTable:nth-of-type(1) td:nth-of-type(3)'
+  },
+  amount: {
+    css: 'table.dataTable:nth-of-type(1) td:nth-of-type(4)'
+  }
+};
+
+module.exports = {
+  table: tableLocators,
+  header: headerLocators,
+  row: rowLocators
+};
