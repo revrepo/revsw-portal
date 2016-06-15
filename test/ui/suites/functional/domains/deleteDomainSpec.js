4e846bca63295598b3fe358624782b72f0b658d4
QAutomation <auto_qa@outlook.com>
2016-06-15 12:00:00 +0300

    fixed Todo for the functional/domains/deleteDomainSpec and switched to new sidebar nav

--- test/ui/suites/functional/domains/deleteDomainSpec.js
+++ test/ui/suites/functional/domains/deleteDomainSpec.js
@@ -27,26 +27,25 @@ describe('Functional', function () {
     var admin = config.get('portal.users.admin');
 
     beforeAll(function () {
+      Portal.signIn(admin);
     });
 
     afterAll(function () {
+      Portal.signOut();
     });
 
     beforeEach(function () {
-      // TODO: Move signIn and signOut calls to beforeAll and afterAll once bug
-      // about creating consecutive domains is fixed
-      Portal.signIn(admin);
-      Portal.getDomainsPage();
+      Portal.selectDomainsItem();
     });
 
     afterEach(function () {
-      Portal.signOut();
+
     });
 
     it('should delete successfully a domain',
       function () {
         var domain = DataProvider.generateDomain('domain-01');
-        Portal.createDomain(domain);
+        Portal.selectDomainsItemAndCreateDomain(domain);
         Portal.domains.listPage.searchAndClickDelete(domain.name);
         Portal.dialog.clickOk();
         Portal.domains.listPage.searcher.setSearchCriteria(domain.name);
@@ -57,7 +56,7 @@ describe('Functional', function () {
     it('should confirm domain deletion when clicking "Ok" button',
       function () {
         var domain = DataProvider.generateDomain('domain-02');
-        Portal.createDomain(domain);
+        Portal.selectDomainsItemAndCreateDomain(domain);
         Portal.domains.listPage.searchAndClickDelete(domain.name);
         var okBtn = Portal.dialog.getOkBtn();
         expect(okBtn.isDisplayed()).toBeTruthy();
@@ -70,26 +69,26 @@ describe('Functional', function () {
     it('should cancel domain deletion when clicking "Cancel" button',
       function () {
         var domain = DataProvider.generateDomain('domain-03');
-        Portal.createDomain(domain);
+        Portal.selectDomainsItemAndCreateDomain(domain);
         Portal.domains.listPage.searchAndClickDelete(domain.name);
         var cancelBtn = Portal.dialog.getCancelBtn();
         expect(cancelBtn.isDisplayed()).toBeTruthy();
         Portal.dialog.clickCancel();
         var domainTableRows = Portal.domains.listPage.table.getRows();
         expect(domainTableRows.count()).toEqual(1);
-        Portal.deleteDomain(domain);
+        Portal.selectDomainsItemAndDeleteDomain(domain);
       });
 
     it('should cancel domain deletion after pressing "ESCAPE" key',
       function () {
         var domain = DataProvider.generateDomain('domain-04');
-        Portal.createDomain(domain);
+        Portal.selectDomainsItemAndCreateDomain(domain);
         Portal.domains.listPage.searchAndClickDelete(domain.name);
         expect(Portal.dialog.getModalEl().isPresent()).toBeTruthy();
         Portal.dialog.getModalEl().sendKeys(protractor.Key.ESCAPE);
         var tableRows = Portal.domains.listPage.table.getRows();
         expect(tableRows.count()).toEqual(1);
-        Portal.deleteDomain(domain);
+        Portal.selectDomainsItemAndDeleteDomain(domain);
       });
   });
 });
