aa26cb670abc4b8c5c1e95865a31d772c366ebda
QAutomation <auto_qa@outlook.com>
2016-06-17 13:55:06 +0300

    removed extra code

--- test/ui/page_objects/portal.js
+++ test/ui/page_objects/portal.js
@@ -358,61 +358,6 @@ var Portal = {
         Constants.sideBar.menu.USERS);
   },
 
-/*  /!**
-   * ### Portal.selectUsersItem()
-   *
-   * Navigates to Users Page avoiding direct link browsing
-   *
-   * @returns {Promise}
-   *!/
-  selectUsersItem: function () {
-    return this
-      .sideBar.selectItemFromExpandedBlock(Constants.header.appMenu.ACCOUNT_SETTINGS,
-        Constants.sideBar.menu.USERS);
-  },*/
-
-/*  /!**
-   * ### Portal.selectUpdatePasswordItem()
-   *
-   * Navigates to Update Password avoiding direct link browsing
-   * and without extra click on menu header
-   *
-   * @returns {Promise}
-   *!/
-  selectUpdatePasswordItem: function () {
-    return this
-      .sideBar.selectItemFromExpandedBlock(Constants.header.appMenu.ACCOUNT_SETTINGS,
-        Constants.sideBar.menu.UPDATE_PASSWORD);
-  },*/
-
-/*  /!**
-   * ### Portal.selectSecuritySettingsItem()
-   *
-   * Navigates to Security settings avoiding direct link browsing
-   * and without extra click on menu header
-   *
-   * @returns {Promise}
-   *!/
-  selectSecuritySettingsItem: function () {
-    return this
-      .sideBar.selectItemFromExpandedBlock(Constants.header.appMenu.ACCOUNT_SETTINGS,
-        Constants.sideBar.menu.SECURITY_SETTINGS);
-  },*/
-
-/*  /!**
-   * ### Portal.selectDomainsItem()
-   *
-   * Navigates to Domains avoiding direct link browsing
-   * and without extra click on menu header
-   *
-   * @returns {Promise}
-   *!/
-  selectDomainsItem: function () {
-    return this
-      .sideBar.selectItemFromExpandedBlock(Constants.header.appMenu.WEB,
-        Constants.sideBar.web.DOMAINS);
-  },*/
-
   /**
    * ### Portal.goToMobileApps()
    *
@@ -522,33 +467,6 @@ var Portal = {
     });
   },
 
-/*  /!**
-   * ### Portal.selectUsersItemAndCreateUser()
-   *
-   * Helper method that navigates to Users page avoiding direct link browsing
-   * and executes all steps required to create a new User from
-   * Portal app.
-   *
-   * @param {user} newUser, data applying the schema defined in
-   * `DataProvider.generateUser()`
-   *
-   * @returns {Object} Promise
-   *!/
-  selectUsersItemAndCreateUser: function (newUser) {
-    var me = this;
-    return browser.getCurrentUrl().then(function (initialUrl) {
-      me.selectUsersItem();
-      me.userListPage.clickAddNewUserThroughClassName();
-      me.addUserPage.createUser(newUser);
-      me.addUserPage.clickBackToList();
-      browser.getCurrentUrl().then(function (currentUrl) {
-        if (initialUrl !== currentUrl) {
-          browser.get(initialUrl);
-        }
-      });
-    });
-  },*/
-
   /**
    * ### Portal.createUserIfNotExist()
    *
@@ -614,36 +532,6 @@ var Portal = {
     });
   },
 
-/*  /!**
-   * ### Portal.selectUsersItemAndDeleteUser()
-   *
-   * Helper method that executes all steps required to delete a User from
-   * Portal app avoiding direct link browsing.
-   *
-   * @param {Object} user, data applying the schema defined in
-   * `DataProvider.generateUser()`
-   *
-   * @returns {Object} Promise
-   *!/
-  selectUsersItemAndDeleteUser: function (user) {
-    var me = this;
-    return browser.getCurrentUrl().then(function (initialUrl) {
-      me.selectUsersItem();
-      me.userListPage.searcher.clearSearchCriteria();
-      me.userListPage.searcher.setSearchCriteria(user.email);
-      me.userListPage.table
-        .getFirstRow()
-        .clickDelete();
-      me.dialog.clickOk();
-      me.userListPage.searcher.clearSearchCriteria();
-      browser.getCurrentUrl().then(function (currentUrl) {
-        if (initialUrl !== currentUrl) {
-          browser.get(initialUrl);
-        }
-      });
-    });
-  },*/
-
   /**
    * ### Portal.createDomain()
    *
@@ -669,32 +557,6 @@ var Portal = {
       });
     });
   },
-
-/*  /!**
-   * ### Portal.selectDomainsItemAndCreateDomain()
-   *
-   * Helper method that executes all steps required to create a new Domain from
-   * Portal app avoiding direct link browsing
-   *
-   * @param {Object} newDomain, data applying the schema defined in
-   * `DataProvider.generateDomain()`
-   *
-   * @returns {Object} Promise
-   *!/
-  selectDomainsItemAndCreateDomain: function (newDomain) {
-    var me = this;
-    return browser.getCurrentUrl().then(function (initialUrl) {
-      me.selectDomainsItem();
-      me.domains.listPage.clickAddNewDomain();
-      me.domains.addPage.createDomain(newDomain);
-      me.domains.addPage.clickBackToList();
-      browser.getCurrentUrl().then(function (currentUrl) {
-        if (initialUrl !== currentUrl) {
-          browser.get(initialUrl);
-        }
-      });
-    });
-  },*/
 
   /**
    * ### Portal.createDomainIfNotExist()
@@ -787,36 +649,6 @@ var Portal = {
       });
     });
   },
-
-/*  /!**
-   * ### Portal.selectDomainsItemAndDeleteDomain()
-   *
-   * Helper method that executes all steps required to delete a Domain from
-   * Portal app avoiding direct link navigation
-   *
-   * @param {Object} domain, data applying the schema defined in
-   * `DataProvider.generateDomain()`
-   *
-   * @returns {Object} Promise
-   *!/
-  selectDomainsItemAndDeleteDomain: function (domain) {
-    var me = this;
-    return browser.getCurrentUrl().then(function (initialUrl) {
-      me.selectDomainsItem();
-      me.domains.listPage.searcher.clearSearchCriteria();
-      me.domains.listPage.searcher.setSearchCriteria(domain.name);
-      me.domains.listPage.table
-        .getFirstRow()
-        .clickDelete();
-      me.dialog.clickOk();
-      me.domains.listPage.searcher.clearSearchCriteria();
-      browser.getCurrentUrl().then(function (currentUrl) {
-        if (initialUrl !== currentUrl) {
-          browser.get(initialUrl);
-        }
-      });
-    });
-  },*/
 
   /**
    * ### Portal.createMobileApps()
