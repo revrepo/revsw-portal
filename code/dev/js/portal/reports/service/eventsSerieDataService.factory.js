65818b1b3d07a7b39b6849e1b1cc4df0bf378bfd
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2016-07-04 12:30:52 +0700

    Save current changes

--- code/dev/js/portal/reports/service/eventsSerieDataService.factory.js
+++ code/dev/js/portal/reports/service/eventsSerieDataService.factory.js
@@ -12,7 +12,7 @@
     .module('revapm.Portal.Reports')
     .factory('EventsSerieDataService', EventsSerieDataService);
 
-  function EventsSerieDataService($q, Activity, ActivityPhrase, $uibModal) {
+  function EventsSerieDataService($q, Activity, ActivityPhrase, DomainsConfig, $uibModal) {
     'ngInject';
     // TODO: rebase to config
     var constEventsColor = {
@@ -24,8 +24,15 @@
     return {
       getEventsSerieDataForDomain: getEventsSerieDataForDomain
     };
-
+    /**
+     * @name  getEventsSerieDataForDomain
+     * @description
+     *
+     * @param  {Object} options
+     * @return {Promise}
+     */
     function getEventsSerieDataForDomain(options) {
+      var promises = [];
       var queryParams = {
         from_timestamp: options.from_timestamp,
         to_timestamp: options.to_timestamp
@@ -35,91 +42,159 @@
       //   queryParams.account_id = options.account_id;
       // }
 
-      return Activity.query(queryParams).$promise
-        .then(function(data) {
-          // NOTE: Activity series
-          var serie = {
-            name: 'Events',
-            data: [],
-            type: 'flags', //'scatter',
-            // color: '#333333',
-            shape: 'circlepin',
-            states: {
-              hover: {
-                fillColor: '#faa947' // darker
-              }
-            },
-            events: {
-              hide: function() {},
-              show: function() {}
-            },
-            point: {
-              events: {
-                click: function() {
-                  showEventDetails(this.options.total);
-                }
-              }
-            },
-            tooltip: {
-              headerFormat: '',
-              pointFormatter: function eventPointFormatter() {
-                var _text = 'Event ';
-                switch (this.options.name) {
-                  case 'purge':
-                  case 'domain':
-                  case 'sslcert':
-                    _text = '<b>' + this.series.name + '</b> ' + ActivityPhrase.EVENT_TYPES[this.options.name] +
-                      '<br>' + moment(this.x).format('[<span style="color: #000; font-weight: bold;">]HH:mm[</span>] MMM D') + '';
-                    _text += '<br> performed by ' + this.total.user_name + '';
-                    break;
-                  default:
-                    _text = '<b>' + 'Event ' + ' </b> ' + ActivityPhrase.EVENT_TYPES[this.options.name];
-                    break;
-                }
-                return _text;
-              }
-            },
-            // showInLegend: false,
-            // style: { // text style
-            //   color: 'white'
-            // },
-          };
-          // - Cache purges
-          // - Domain configuration changes (publishes)
-          // - SSL Cert
-          angular.forEach(data.data, function(item) {
-            if (item.activity_target === 'purge' ||
-              (item.activity_target === 'domain' && item.activity_type === 'publish') ||
-              item.activity_target === 'sslcert') {
-              // Skip events not for domain
-              if (!!options.domain_name) {
-                if (((item.activity_target === 'domain' && item.activity_type === 'publish') ||
-                    item.activity_target === 'purge') && item.target_name !== options.domain_name) {
-                  return;
-                }
-              }
-              // Set color for activity_target
-              var marker = {
-                fillColor: constEventsColor[item.activity_target],
-                radius: 6
+      /**
+       * @actionPreparingSSLCertEventsRequest
+       * @description
+       *   Preparing request for get Events(Activity) of target type "SSL Certificates"
+       * @return {Promise}
+       */
+      function actionPreparingSSLCertEventsRequest(options, promises) {
+        var def = $q.defer();
+        if (!!options.domain_id) {
+          DomainsConfig.get({
+            id: options.domain_id
+          }).$promise.then(function(domainConfig) {
+            if (domainConfig.ssl_cert_id && domainConfig.ssl_cert_id !== '') {
+              var sslCertQueryPrams = {
+                from_timestamp: options.from_timestamp,
+                to_timestamp: options.to_timestamp,
+                target_id: domainConfig.ssl_cert_id,
+                target_type: 'sslcert'
               };
-              var eventPointData = {
-                id: item.activity_target + '_' + item.datetime,
-                name: item.activity_target,
-                activity_target: item.activity_target,
-                user_type: item.user_type,
-                total: item,
-                title: item.activity_target[0].toUpperCase(),
-                y: 0,
-                x: item.datetime,
-                marker: marker
-              };
-              serie.data.push(eventPointData);
+              def.resolve(Activity.query(sslCertQueryPrams).$promise);
             }
+            def.resolve($q.when(null));
           });
-          return $q.when(serie);
-        }, function() {
-          return $q.when(serie);
+        } else {
+          def.resolve($q.when(null));
+        }
+        return def.promise;
+      }
+      /**
+       * @name  actionPreparingDomainEventsRequest
+       * @description
+       *
+       * @param  {Object} options
+       * @param  {[type]} promises [description]
+       * @return {[type]}          [description]
+       */
+      function actionPreparingDomainEventsRequest(options, promises) {
+        var def = $q.defer();
+        if (!!options.domain_id) {
+          var domainQueryPrams = {
+            from_timestamp: options.from_timestamp,
+            to_timestamp: options.to_timestamp,
+            target_id: options.domain_id,
+            target_type: 'domain'
+          };
+          def.resolve(Activity.query(domainQueryPrams).$promise);
+
+        } else {
+          def.resolve($q.when(null));
+        }
+        return def.promise;
+      }
+      return $q.all([
+          actionPreparingSSLCertEventsRequest(options, promises),
+          actionPreparingDomainEventsRequest(options, promises)
+        ])
+        .then(function(data) {
+          console.log('all actinon preparing complite', data);
+          return $q.all(promises).then(function(results) {
+            return results;
+          });
+        })
+        .then(function(data) {
+          // NOTE: create series data
+          console.log('total', data);
+
+
+          return Activity.query(queryParams).$promise
+            .then(function(data) {
+              // NOTE: Activity series
+              var serie = {
+                name: 'Events',
+                data: [],
+                type: 'flags', //'scatter',
+                // color: '#333333',
+                shape: 'circlepin',
+                states: {
+                  hover: {
+                    fillColor: '#faa947' // darker
+                  }
+                },
+                events: {
+                  hide: function() {},
+                  show: function() {}
+                },
+                point: {
+                  events: {
+                    click: function() {
+                      showEventDetails(this.options.total);
+                    }
+                  }
+                },
+                tooltip: {
+                  headerFormat: '',
+                  pointFormatter: function eventPointFormatter() {
+                    var _text = 'Event ';
+                    switch (this.options.name) {
+                      case 'purge':
+                      case 'domain':
+                      case 'sslcert':
+                        _text = '<b>' + this.series.name + '</b> ' + ActivityPhrase.EVENT_TYPES[this.options.name] +
+                          '<br>' + moment(this.x).format('[<span style="color: #000; font-weight: bold;">]HH:mm[</span>] MMM D') + '';
+                        _text += '<br> performed by ' + this.total.user_name + '';
+                        break;
+                      default:
+                        _text = '<b>' + 'Event ' + ' </b> ' + ActivityPhrase.EVENT_TYPES[this.options.name];
+                        break;
+                    }
+                    return _text;
+                  }
+                },
+                // showInLegend: false,
+                // style: { // text style
+                //   color: 'white'
+                // },
+              };
+              // - Cache purges
+              // - Domain configuration changes (publishes)
+              // - SSL Cert
+              angular.forEach(data.data, function(item) {
+                if (item.activity_target === 'purge' ||
+                  (item.activity_target === 'domain' && item.activity_type === 'publish') ||
+                  item.activity_target === 'sslcert') {
+                  // Skip events not for domain
+                  if (!!options.domain_name) {
+                    if (((item.activity_target === 'domain' && item.activity_type === 'publish') ||
+                        item.activity_target === 'purge') && item.target_name !== options.domain_name) {
+                      return;
+                    }
+                  }
+                  // Set color for activity_target
+                  var marker = {
+                    fillColor: constEventsColor[item.activity_target],
+                    radius: 6
+                  };
+                  var eventPointData = {
+                    id: item.activity_target + '_' + item.datetime,
+                    name: item.activity_target,
+                    activity_target: item.activity_target,
+                    user_type: item.user_type,
+                    total: item,
+                    title: item.activity_target[0].toUpperCase(),
+                    y: 0,
+                    x: item.datetime,
+                    marker: marker
+                  };
+                  serie.data.push(eventPointData);
+                }
+              });
+              return $q.when(serie);
+            }, function() {
+              return $q.when(serie);
+            });
         });
     }
 
