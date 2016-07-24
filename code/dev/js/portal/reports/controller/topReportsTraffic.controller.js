261b6d8effae2d70bea6b2d4ee5deb55221c6531
Victor Gartvich <victor@revsw.com>
2016-07-24 02:53:49 +0000

    Polishing the mobile/desktop pie chart

--- code/dev/js/portal/reports/controller/topReportsTraffic.controller.js
+++ code/dev/js/portal/reports/controller/topReportsTraffic.controller.js
@@ -344,8 +344,8 @@
           $scope.mobileDesktopRatio = [
             { name: 'Mobile', y: data.data.mobile },
             { name: 'Desktop', y: data.data.desktop },
-            { name: 'Spiders', y: data.data.spiders }
-          ]
+            { name: 'Web Crawlers', y: data.data.spiders }
+          ];
         })
         .catch(function() {
           $scope.mobileDesktopRatio = [];
