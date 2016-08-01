2a6b9e1dcf27aeca626d9fa591e852e20a4f37c2
dmitry_t <dmitry@mainlywrenches.co>
2016-08-01 20:37:44 +0400

    Usage Repord, Domains Usage UI tests updated

--- test/ui/suites/functional/billing/usageReportDomainsSpec.js
+++ test/ui/suites/functional/billing/usageReportDomainsSpec.js
@@ -25,121 +25,149 @@ describe('Functional', function () {
 
     var user = config.get('portal.users.admin');
     var USAGE_REPORT = Constants.sideBar.billing.USAGE_REPORT;
-    var testDomain = config.get('portal.usageReport.testDomain');
+
+    var testDomain = 'www.google-test.com';
+    var testDomains = config.get('portal.usageReport.testDomains');
+    var testDomainsNum = testDomains.length;
+    var denom = parseInt( config.get('portal.usageReport.hits_denominator') );
 
     //  cached
     var rows, tds, row1, totalHits;
 
     beforeAll(function () {
       Portal.signIn(user);
-      Portal.goToBilling();
-      Portal.header.goTo(USAGE_REPORT);
+      Portal.goToUsageReport();
+      // Portal.goToBilling();
+      // Portal.header.goTo(USAGE_REPORT);
     });
 
     afterAll(function () {
       Portal.signOut();
     });
 
+
+    // it('should get some Usage Report page', function() {
+    //   var tr = Portal.billing.usageReportDomainsPage.getSome();
+    //   expect(tr.count()).toEqual( 4 );
+
+    //   // var some = tr.all(by.css('td')).get(1).getText();
+    //   // expect(some).toEqual( testDomain );
+
+    //   // var tr = Portal.billing.usageReportDomainsPage.getNamedTr( testDomains[0] );
+    //   // var t = tr.getText();
+    //   // expect(t).toEqual( 'no' );
+
+    //   // var some = tr.all(by.css('td')).get(1).getText();
+    //   // expect(some).toEqual( testDomain );
+    // });
+
+
     //  ---------------------------------
-    it('should contain testing domain data', function() {
-      rows = Portal.billing.usageReportDomainsPage.getDomainRows( testDomain );
-      expect(rows.count()).toEqual(2);
+    it('should get title "Domains Usage" from Usage Report page', function() {
+      var title = Portal.billing.usageReportDomainsPage.getTitle();
+      expect(title).toEqual('Domains Usage');
+    });
+
+    //  ---------------------------------
+    it('should contain testing domains data', function() {
+      rows = testDomains.map( function( domain ) {
+        var r = Portal.billing.usageReportDomainsPage.getDomainRows( domain );
+        var tds = r.get(0).all(by.css('td'));
+        expect(tds.count()).toEqual(7);
+        return {
+          domain: domain,
+          rows: r,
+          tds: tds
+        }
+      });
+      expect(rows.length).toEqual(testDomains.length);
     });
 
     //  ---------------------------------
     it('should contain correct total hits num', function() {
 
-      tds = rows.get(0).all(by.css('td'));
-      tds.get(2)
-        .getText()
-        .then( function( hits ) {
-          totalHits = parseInt( hits.replace( /'/g, '' ) );
-          var denom = parseInt( config.get('portal.usageReport.hits_denominator') );
-          expect(totalHits % denom ).toEqual(0);
-        });
+      rows.forEach( function( r ) {
+        r.tds.get(2).getText()
+          .then( function( hits ) {
+            r.hits = parseInt( hits.replace( /'/g, '' ) );
+            expect( r.hits ).not.toEqual( 0 );
+            expect( r.hits % denom ).toEqual( 0 );
+          });
+      });
+
     });
 
     //  ---------------------------------
     it('should contain correct sent traffic volume', function() {
 
-      tds.get(3)
-        .getText()
-        .then( function( sent ) {
-          sent = sent.slice(0,-3).replace( /'/g, '' );
-          // console.log( 'sent ' + sent );
-          var unit = parseInt( config.get( 'portal.usageReport.sent_unit' ) );
-          var sentEst = totalHits * unit / 1024 / 1024 / 1024;
-          expect(sentEst.toFixed(1)).toEqual(sent);
-        });
+      var unit = parseInt( config.get( 'portal.usageReport.sent_unit' ) );
+      rows.forEach( function( r ) {
+        r.tds.get(3).getText()
+          .then( function( sent ) {
+            sent = parseFloat( sent.slice(0,-3).replace( /'/g, '' ) );
+            r.sent = Math.round( r.hits * unit / 1024 / 1024 / 1024 * 1000 ) / 1000;
+            expect(r.sent).toEqual(sent);
+          });
+      });
 
     });
 
     //  ---------------------------------
     it('should contain correct received traffic volume', function() {
 
-      tds.get(4)
-        .getText()
-        .then( function( received ) {
-          received = received.slice(0,-3).replace( /'/g, '' );
-          // console.log( 'received ' + received );
-          var unit = parseInt( config.get( 'portal.usageReport.received_unit' ) );
-          var receivedEst = totalHits * unit / 1024 / 1024 / 1024;
-          expect(receivedEst.toFixed(2)).toEqual(received);
-        });
+      var unit = parseInt( config.get( 'portal.usageReport.received_unit' ) );
+      rows.forEach( function( r ) {
+        r.tds.get(4).getText()
+          .then( function( received ) {
+            received = parseFloat( received.slice(0,-3).replace( /'/g, '' ) );
+            r.received = Math.round( r.hits * unit / 1024 / 1024 / 1024 * 1000 ) / 1000;
+            expect(r.received).toEqual(received);
+          });
+      });
+
     });
 
     //  ---------------------------------
-    it('should contain correct cache-miss hits number', function() {
+    it('should contain correct cache HIT/MISS hits number', function() {
 
-      tds.get(0)
-        .element(by.css('a'))
-        .click()
-        .then( function() {
-          row1 = rows.get(1);
-          row1.element(by.id('cache_miss'))
-            .getText()
-            .then( function( hits ) {
-              hits = parseInt( hits.replace( /'/g, '' ) );
-              expect( hits ).toEqual( totalHits / 2 );
-            });
-        });
+      rows.forEach( function( r ) {
+        r.tds.get(0).element(by.css('a')).click()
+          .then( function() {
+            r.rows.get(1).element(by.id('cache_miss')).getText()
+              .then( function( hits ) {
+                hits = parseInt( hits.replace( /'/g, '' ) );
+                expect( hits ).toEqual( r.hits / 2 );
+              });
+            r.rows.get(1).element(by.id('cache_hit')).getText()
+              .then( function( hits ) {
+                hits = parseInt( hits.replace( /'/g, '' ) );
+                expect( hits ).toEqual( r.hits / 2 );
+              });
+          });
+      });
+
     });
 
     //  ---------------------------------
-    it('should contain correct cache-hit hits number', function() {
+    it('should contain correct HTTP/HTTPS hits number', function() {
 
-      row1.element(by.id('cache_hit'))
-        .getText()
-        .then( function( hits ) {
-          hits = parseInt( hits.replace( /'/g, '' ) );
-          expect( hits ).toEqual( totalHits / 2 );
-        });
+      rows.forEach( function( r ) {
+        //  must be clicked in the previous test:
+        // r.tds.get(0).element(by.css('a')).click()
+        //   .then( function() { .........
+        r.rows.get(1).element(by.id('port_80')).getText()
+          .then( function( hits ) {
+            hits = parseInt( hits.replace( /'/g, '' ) );
+            expect( hits ).toEqual( r.hits / 2 );
+          });
+        r.rows.get(1).element(by.id('port_443')).getText()
+          .then( function( hits ) {
+            hits = parseInt( hits.replace( /'/g, '' ) );
+            expect( hits ).toEqual( r.hits / 2 );
+          });
+      });
+
     });
-
-    //  ---------------------------------
-    it('should contain correct HTTP hits number', function() {
-
-      row1.element(by.id('port_80'))
-        .getText()
-        .then( function( hits ) {
-          hits = parseInt( hits.replace( /'/g, '' ) );
-          // console.log( hits );
-          expect( hits ).toEqual( totalHits / 2 );
-        });
-    });
-
-    //  ---------------------------------
-    it('should contain correct HTTPS hits number', function() {
-
-      row1.element(by.id('port_443'))
-        .getText()
-        .then( function( hits ) {
-          hits = parseInt( hits.replace( /'/g, '' ) );
-          // console.log( hits );
-          expect( hits ).toEqual( totalHits / 2 );
-        });
-    });
-
 
   });
 });
