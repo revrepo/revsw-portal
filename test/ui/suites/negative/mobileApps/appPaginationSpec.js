d417873f90476bdb63a86a7be51ff95f92f0afbc
Leo <leo.fcx@gmail.com>
2016-10-17 20:08:09 -0400

    Fix negative mobile apps specs for pagination

--- test/ui/suites/negative/mobileApps/appPaginationSpec.js
+++ test/ui/suites/negative/mobileApps/appPaginationSpec.js
@@ -18,76 +18,79 @@
 
 var config = require('config');
 var Portal = require('./../../../page_objects/portal');
-var DataProvider = require('./../../../common/providers/data');
-var Constants = require('./../../../page_objects/constants');
 
 describe('Negative', function () {
   describe('Apps Pagination', function () {
 
-    var user = config.get('portal.users.admin');
-    var platforms = Portal.constants.mobileApps.platforms;
-    var iosApps = DataProvider.generateMobileAppData(platforms.ios, 1);
-    var androidApps = DataProvider.generateMobileAppData(platforms.android, 1);
-    var apps = iosApps.concat(androidApps);
+    var users = [
+      config.get('portal.users.admin')
+    ];
+    var platforms = [
+      Portal.constants.mobileApps.platforms.android,
+      Portal.constants.mobileApps.platforms.ios
+    ];
 
-    apps.forEach(function (app) {
+    users.forEach(function (user) {
 
-      describe('Platform: ' + app.platform, function () {
+      describe('With user: ' + user.role, function () {
 
-        beforeAll(function () {
-          Portal.signIn(user);
-        });
+        platforms.forEach(function (platform) {
 
-        afterAll(function () {
-          Portal.signOut();
-        });
+          describe('For platform: ' + platform, function () {
 
-        beforeEach(function () {
-          Portal.helpers.nav.goToMobileAppsMenuItem(platform);
-        });
+            beforeAll(function () {
+              Portal.signIn(user);
+            });
 
-        afterEach(function () {
-          Portal.mobileApps.listPage.searcher.clearSearchCriteria();
-        });
+            afterAll(function () {
+              Portal.signOut();
+            });
 
-        it('should not display pagination when there is not any app to show',
-          function () {
-            var uniqueString = 'unique_string_' + (new Date()).getTime();
-            Portal.mobileApps.listPage.setSearch(uniqueString);
-            expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
+            beforeEach(function () {
+              Portal.helpers.nav.goToMobileAppsMenuItem(platform);
+            });
+
+            afterEach(function () {
+              Portal.mobileApps.listPage.searcher.clearSearchCriteria();
+            });
+
+            it('should not display pagination when there is not any app to ' +
+              'show', function () {
+              var uniqueString = 'unique_string_' + (new Date()).getTime();
+              Portal.mobileApps.listPage.setSearch(uniqueString);
+              expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
+            });
+
+            it('should not show pagination buttons when the search criteria ' +
+              'does not match with any word in the `App List`', function () {
+              var uniqueString = 'unique_string_' + (new Date()).getTime();
+              var availablePages = Portal.mobileApps.listPage.pager
+                .getAllPageIndexButtons()
+                .count();
+              Portal.mobileApps.listPage.setSearch(uniqueString);
+              var filteredPages = Portal.mobileApps.listPage.pager
+                .getAllPageIndexButtons()
+                .count();
+              expect(availablePages).toBeGreaterThan(1);
+              expect(filteredPages).toEqual(0);
+              expect(availablePages).toBeGreaterThan(filteredPages);
+            });
+
+            it('should display the `Previous Page` disabled after filtering ' +
+              'does not return any apps in the `App List`', function () {
+              var uniqueString = 'unique_string_' + (new Date()).getTime();
+              Portal.mobileApps.listPage.setSearch(uniqueString);
+              expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
+            });
+
+            it('should not display the `Next Page` after filtering does not ' +
+              'return any apps in the `App List`', function () {
+              var uniqueString = 'unique_string_' + (new Date()).getTime();
+              Portal.mobileApps.listPage.setSearch(uniqueString);
+              expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
+            });
           });
-
-        it('should not show pagination buttons when the search criteria does' +
-          'not match with any word in the `App List`',
-          function () {
-            var uniqueString = 'unique_string_' + (new Date()).getTime();
-            var availablePages = Portal.mobileApps.listPage.pager
-              .getAllPageIndexButtons()
-              .count();
-            Portal.mobileApps.listPage.setSearch(uniqueString);
-            var filteredPages = Portal.mobileApps.listPage.pager
-              .getAllPageIndexButtons()
-              .count();
-            expect(availablePages).toBeGreaterThan(1);
-            expect(filteredPages).toEqual(0);
-            expect(availablePages).toBeGreaterThan(filteredPages);
-          });
-
-        it('should display the `Previous Page` disabled after filtering does not ' +
-          'return any apps in the `App List`',
-          function () {
-            var uniqueString = 'unique_string_' + (new Date()).getTime();
-            Portal.mobileApps.listPage.setSearch(uniqueString);
-            expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
-          });
-
-        it('should not display the `Next Page` after filtering does not ' +
-          'return any apps in the `App List`',
-          function () {
-            var uniqueString = 'unique_string_' + (new Date()).getTime();
-            Portal.mobileApps.listPage.setSearch(uniqueString);
-            expect(Portal.mobileApps.listPage.pager.isDisplayed()).toBe(false);
-          });
+        });
       });
     });
   });
