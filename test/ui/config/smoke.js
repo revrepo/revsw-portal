636774cd7aa34e6ebf0023745031bf84af5b09cb
Leo <leo.fcx@gmail.com>
2016-10-27 10:25:29 -0400

    Adding failing specs to execute

--- test/ui/config/smoke.js
+++ test/ui/config/smoke.js
@@ -1,7 +1,10 @@
 var smoke = require('./base');
 
 smoke.specs = [
-  '../suites/smoke/**/*Spec.js'
+  '../suites/smoke/users/addUserSpec.js',
+  '../suites/smoke/accounts/accountBillingStatementsSpec.js',
+  '../suites/smoke/dnsZoneRecords/editZoneRecordSpec.js',
+  '../suites/smoke/sslNames/addSSLNameSpec.js'
 ];
 
 exports.config = smoke;
