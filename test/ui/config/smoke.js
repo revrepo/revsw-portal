ec5d30c630df17f5b65f6b59e49dbd6af12539ff
Leo <leo.fcx@gmail.com>
2016-11-05 09:24:44 -0400

    Executing all test in one

--- test/ui/config/smoke.js
+++ test/ui/config/smoke.js
@@ -5,16 +5,8 @@ smoke.capabilities = {
 };
 
 smoke.specs = [
-  '../suites/smoke/accounts/*Spec.js',
-  '../suites/smoke/admin/**/*Spec.js',
-  '../suites/smoke/analytics/*Spec.js',
-  '../suites/smoke/dashboards/*Spec.js',
-  '../suites/smoke/helpSupport/*Spec.js',
-  '../suites/smoke/logShipping/*Spec.js',
-  '../suites/smoke/mobileApps/*Spec.js',
-  '../suites/smoke/stagingEnv/*Spec.js',
-  '../suites/smoke/users/*Spec.js',
-  '../suites/smoke/*Spec.js'
+  '../suites/smoke/*Spec.js',
+  '../suites/smoke/**/*Spec.js'
 ];
 
 exports.config = smoke;
