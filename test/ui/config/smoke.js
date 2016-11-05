00ed82f48dafdcf1f1916c093c64b8d2b9ea2491
Leo <leo.fcx@gmail.com>
2016-11-04 23:01:12 -0400

    Restoring smoke tests to run

--- test/ui/config/smoke.js
+++ test/ui/config/smoke.js
@@ -5,17 +5,16 @@ smoke.capabilities = {
 };
 
 smoke.specs = [
-  // '../suites/smoke/accounts/*Spec.js',
-  // '../suites/smoke/admin/**/*Spec.js',
-  // '../suites/smoke/analytics/*Spec.js',
-  // '../suites/smoke/dashboards/*Spec.js',
-  // '../suites/smoke/helpSupport/*Spec.js',
-  // '../suites/smoke/logShipping/*Spec.js',
-  // '../suites/smoke/mobileApps/*Spec.js',
-  // '../suites/smoke/stagingEnv/*Spec.js',
-  // '../suites/smoke/users/*Spec.js',
-  // '../suites/smoke/*Spec.js'
-  '../suites/smoke/sslNames/*Spec.js'
+  '../suites/smoke/accounts/*Spec.js',
+  '../suites/smoke/admin/**/*Spec.js',
+  '../suites/smoke/analytics/*Spec.js',
+  '../suites/smoke/dashboards/*Spec.js',
+  '../suites/smoke/helpSupport/*Spec.js',
+  '../suites/smoke/logShipping/*Spec.js',
+  '../suites/smoke/mobileApps/*Spec.js',
+  '../suites/smoke/stagingEnv/*Spec.js',
+  '../suites/smoke/users/*Spec.js',
+  '../suites/smoke/*Spec.js'
 ];
 
 exports.config = smoke;
