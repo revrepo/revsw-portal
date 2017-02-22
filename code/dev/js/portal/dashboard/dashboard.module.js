88d8528cba5527f43d93a98ee34eac4d95d395b5
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2017-02-22 12:22:40 +0700

    Add new widgets with Mobile Apps analytics

--- code/dev/js/portal/dashboard/dashboard.module.js
+++ code/dev/js/portal/dashboard/dashboard.module.js
@@ -15,6 +15,7 @@
       'adf.provider',
       'revapm.Portal.BlueTriangleTech',
       'angular-nicescroll',
+      'revapm.Portal.Dashboard.Widgets.AppsAnalytics',
       'revapm.Portal.Dashboard.Widgets.WebAnalytics',
       'adf.widget.analytics-proxy-traffic'
     ])
@@ -86,136 +87,6 @@
             }]
           }]
         });
-    })
-    // TODO: rebase
-    .config( /*ngInject*/ function(dashboardProvider) {
-      dashboardProvider
-      // #1  “Conversion Rate”
-        .widget('bluetriangletech-conversions-subcategories', {
-          title: 'Conversion Rate',
-          description: 'Conversion Rate, Average Order Value and Page Views by PRT for Subcategories',
-          templateUrl: 'parts/dashboard/widgets/bluetriangletech/view-iframe-bluetriangletech.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/bluetriangletech/widget-title-with-params-bluetriangletech.html',
-          controller: 'widgetBTTConversionReportController',
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/bluetriangletech/edit-bluetriangletech.html',
-            controller: 'widgetEditBTTiframeController',
-            controllerAs: 'vm',
-          },
-          config: {
-            height: '100%',
-            country: 'All Countries',
-            count_last_day: '1'
-          }
-        })
-        // #2 ”Bounce Rate”
-        .widget('bluetriangletech-bounce-rate', {
-          title: 'Bounce Rate',
-          description: 'Bounce Rate and Page Views Per Session by PRT for subcategory',
-          templateUrl: 'parts/dashboard/widgets/bluetriangletech/view-iframe-bluetriangletech.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/bluetriangletech/widget-title-with-params-bluetriangletech.html',
-          controller: 'widgetBTTBounceRateReportController',
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/bluetriangletech/edit-bluetriangletech.html',
-            controller: 'widgetEditBTTiframeController',
-            controllerAs: 'vm',
-          },
-          config: {
-            height: '100%',
-            className: 'bluetriangletech--bounce-rate',
-            country: 'All Countries',
-            count_last_day: '1'
-          }
-        })
-        // #3 'Brand Conversion Rate'
-        .widget('bluetriangletech-brand-conversion-rate', {
-          title: 'Brand Conversion Rate',
-          description: 'Brand Conversion Rate, Average Brand and Page Views by PRT for subcategory',
-          templateUrl: 'parts/dashboard/widgets/bluetriangletech/view-iframe-bluetriangletech.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/bluetriangletech/widget-title-with-params-bluetriangletech.html',
-          controller: 'widgetBTTBrandConversionRateReportController',
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/bluetriangletech/edit-bluetriangletech.html',
-            controller: 'widgetEditBTTiframeController',
-            controllerAs: 'vm',
-          },
-          config: {
-            height: '100%',
-            className: 'bluetriangletech--brand-conversion-rate',
-            country: 'All Countries',
-            count_last_day: '1'
-          }
-        })
-        // #4 'Lost Revenue Calculator'
-        .widget('bluetriangletech-lost-revenue-calculator', {
-          title: 'Lost Revenue Calculator',
-          description: 'Lost Revenue Calculator for subcategory',
-          templateUrl: 'parts/dashboard/widgets/bluetriangletech/view-iframe-bluetriangletech.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/bluetriangletech/widget-title-with-params-bluetriangletech.html',
-          controller: 'widgetBTTLostRevenueCalculatorReportController',
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/bluetriangletech/edit-bluetriangletech.html',
-            controller: 'widgetEditBTTiframeController',
-            controllerAs: 'vm',
-          },
-          config: {
-            height: '100%',
-            className: 'bluetriangletech--lost-revenue-calculator',
-            country: 'All Countries',
-            count_last_day: '1'
-          }
-        })
-        // #5 'BTT Traffic Parameters'
-        .widget('bluetriangletech-traffic-info', {
-          title: 'BTT Traffic Parameters',
-          description: 'BTT Traffic Parameters',
-          templateUrl: 'parts/dashboard/widgets/bluetriangletech/view-iframe-bluetriangletech.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/bluetriangletech/widget-title-with-params-bluetriangletech.html',
-          controller: 'widgetBTTTrafficInfoReportController',
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/bluetriangletech/edit-bluetriangletech.html',
-            controller: 'widgetEditBTTiframeController',
-            controllerAs: 'vm',
-          },
-          config: {
-            height: '100%',
-            className: 'bluetriangletech--traffic-info',
-            country: 'All Countries',
-            count_last_day: '1'
-          }
-        })
-        // Widget “Norse Live Attack Map”
-        .widget('widget-norse-live-attack-map', {
-          title: 'Norse Live Attack Map',
-          description: 'Display Norse Live Attack Map',
-          templateUrl: 'parts/dashboard/widgets/norse/view-iframe-norse.tpl.html',
-          titleTemplateUrl: 'parts/dashboard/widgets/norse/widget-title-without-params-norse.html',
-          controller: function widgetNorseLiveAttackMapIframeController($sce, config) {
-            'ngInject';
-            var vm = this;
-            if (config.url) {
-              vm.url = $sce.trustAsResourceUrl(config.url);
-            }
-          },
-          controllerAs: 'iframe',
-          edit: {
-            templateUrl: 'parts/dashboard/widgets/norse/edit-norse.html',
-            controller: function widgetEditBTTiframeController($sce, config) {
-              'ngInject';
-              var vm = this;
-            },
-            controllerAs: 'vm',
-          },
-          config: {
-            url: 'https://map.norsecorp.com/#/'
-          }
-        });
-
     })
     .run(setCustomWidgetTemplates)
     .controller('widgetBTTConversionReportController', widgetBTTConversionReportController)
