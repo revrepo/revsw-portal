4b6ebb039262eb55b777a45636c0674a5a466367
oleg <traeop@gmail.com>
2017-03-23 10:30:02 +0200

    Update topReportsPage.js

--- test/ui/page_objects/mobileAnalytics/topReportsPage.js
+++ test/ui/page_objects/mobileAnalytics/topReportsPage.js
@@ -1,1 +1,62 @@
-/************************************************************************* * * REV SOFTWARE CONFIDENTIAL * * [2013] - [2017] Rev Software, Inc. * All Rights Reserved. * * NOTICE:  All information contained herein is, and remains * the property of Rev Software, Inc. and its suppliers, * if any.  The intellectual and technical concepts contained * herein are proprietary to Rev Software, Inc. * and its suppliers and may be covered by U.S. and Foreign Patents, * patents in process, and are protected by trade secret or copyright law. * Dissemination of this information or reproduction of this material * is strictly forbidden unless prior written permission is obtained * from Rev Software, Inc. */// # TopReports Page Object// This `TopReports` Page Object abstracts all operations or actions// that a common TopReports could do in the Portal app/site.var TopReports = {  // ## Properties  // Locators specific to HTML elements from this page object  locators: {    views: {      container: '.container-fluid .row',	}  },  /**   * ### TopReports.getTitleLbl()   *   * Returns the reference to the `Title` label element (Selenium WebDriver   * Element) from the TopReports page from the Portal app.   *   * @returns {Selenium WebDriver Element}   */  getTitleLbl: function() {    return element      .all(by.css(this.locators.views.container))      .get(0);  },  /**   * ### TopReports.getTitle()   *   * Gets the title from `TopReports` Page.   *   * @returns {Promise}   */  getTitle: function () {    return this      .getTitleLbl()      .getText();  }};module.exports = TopReports;
+/************************************************************************
+ *
+ * REV SOFTWARE CONFIDENTIAL
+ *
+ * [2013] - [2017] Rev Software, Inc.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rev Software, Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rev Software, Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rev Software, Inc.
+ */
+
+// # TopReports Page Object
+
+// This `TopReports` Page Object abstracts all operations or actions
+// that a common TopReports could do in the Portal app/site.
+var TopReports = {
+
+  // ## Properties
+
+  // Locators specific to HTML elements from this page object
+  locators: {
+    views: {
+      container: '.container-fluid .row',
+	}
+  },
+
+  /**
+   * ### TopReports.getTitleLbl()
+   *
+   * Returns the reference to the `Title` label element (Selenium WebDriver
+   * Element) from the TopReports page from the Portal app.
+   *
+   * @returns {Selenium WebDriver Element}
+   */
+  getTitleLbl: function() {
+    return element
+      .all(by.css(this.locators.views.container))
+      .get(0);
+  },
+
+  /**
+   * ### TopReports.getTitle()
+   *
+   * Gets the title from `TopReports` Page.
+   *
+   * @returns {Promise}
+   */
+  getTitle: function () {
+    return this
+      .getTitleLbl()
+      .getText();
+  }
+};
+
+module.exports = TopReports;
