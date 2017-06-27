79d12e84e29a00421f18204cbfee50b047ab0b0d
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2017-06-27 14:32:58 +0700

    Update PO Domain form and edit page

--- test/ui/page_objects/domain/form.js
+++ test/ui/page_objects/domain/form.js
@@ -2,7 +2,7 @@
  *
  * REV SOFTWARE CONFIDENTIAL
  *
- * [2013] - [2015] Rev Software, Inc.
+ * [2013] - [2017] Rev Software, Inc.
  * All Rights Reserved.
  *
  * NOTICE:  All information contained herein is, and remains
@@ -414,25 +414,29 @@ var DomainForm = {
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(4)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(4) a'
       },
-      waf: {
+      acl: {
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(5)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(5) a'
       },
-      customVCLRules:{
+      waf: {
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(6)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(6) a'
       },
-      luaScripting:{
+      customVCLRules:{
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(7)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(7) a'
       },
-      thirdPartyLinks:{
+      luaScripting:{
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(8)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(8) a'
       },
-      imageEngine:{
+      thirdPartyLinks:{
         li: '.domain-edit-form .nav.nav-tabs>li:nth-child(9)',
         css: '.domain-edit-form .nav.nav-tabs>li:nth-child(9) a'
+      },
+      imageEngine:{
+        li: '.domain-edit-form .nav.nav-tabs>li:nth-child(10)',
+        css: '.domain-edit-form .nav.nav-tabs>li:nth-child(10) a'
       }
     }
   },
@@ -501,6 +505,17 @@ var DomainForm = {
    */
   getSSLConfigurationTab: function () {
     return element(by.css(this.locators.tabs.sslConfiguration.css));
+  },
+  /**
+   * ### DomainForm.getACLTab()
+   *
+   * Returns the reference to the `WAF` tab (Selenium WebDriver
+   * Element)
+   *
+   * @returns {Selenium WebDriver Element}
+   */
+  getACLTab: function() {
+    return element(by.css(this.locators.tabs.acl.css));
   },
   /**
    * ### DomainForm.getWAFTab()
