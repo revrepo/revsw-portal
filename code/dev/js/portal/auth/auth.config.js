9b608666c3949e2eace18581aae6d1629f84028e
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2017-06-27 15:51:54 +0700

    Update for not send 'Authorization' header for all '//portal' requests

--- code/dev/js/portal/auth/auth.config.js
+++ code/dev/js/portal/auth/auth.config.js
@@ -8,13 +8,19 @@
   /*@ngInject*/
   function configure($httpProvider) {
     // alternatively, register the interceptor via an anonymous factory
-    //$httpProvider.interceptors.push(function($q, dependency1, dependency2) {
-    //  return {
-    //    'request': function(config) {
-    //      //@todo Add header
-    //      //$httpProvider.defaults.headers.some = ;
-    //    }
-    //  };
-    //});
+    // @see https://docs.angularjs.org/api/ng/service/$http
+    $httpProvider.interceptors.push(function($q,$location) {
+      var location = '//portal';//
+      /*@ngInject*/
+      return {
+        'request': function(config) {
+          // NOTE: delete header 'Authorization' with JWT for caching
+          if(config.method === 'GET' && (config.url.indexOf(location)>-1)){
+            delete config.headers.Authorization;
+          }
+          return config;
+        }
+      };
+    });
   }
 })();
