04f29dfae3bd5dfc860491da5485aeb8bc4f367f
AsherMoshav <asher@mediadome.co.il>
2017-08-23 02:27:31 +0300

    OTP wildcard function added check for backspace

--- code/dev/js/portal/auth/login/twoFactorAuthCodeModal.controller.js
+++ code/dev/js/portal/auth/login/twoFactorAuthCodeModal.controller.js
@@ -27,7 +27,7 @@
       AlertService.clear();
       $scope.data.loading = true;
       try {
-        User.login(auth.email, auth.password, this.code.replace(/\D/g,''))
+        User.login(auth.email, auth.password, this.code.replace(/\D/g, ''))
           .then(function (data) {
             $uibModalInstance.close(data);
           })
@@ -50,12 +50,16 @@
     };
 
     // Replace digits with wildcard
-    $scope.wildcard = function (e) {     
-      if (e.target.value.includes(e.key)) {
-        this.code += e.key;
-        setTimeout(function () {
-          e.target.value = e.target.value.replace(e.key, '*');
-        }, $config.OTP_WILDCARD_DELAY);
+    $scope.wildcard = function (e) {      
+      if (e.keyCode == 8 || e.keyCode == 46) {
+        this.code = this.code.slice(0, -1);
+      } else {
+        if (e.target.value.includes(e.key)) {
+          this.code += e.key;
+          setTimeout(function () {
+            e.target.value = e.target.value.replace(e.key, '*');
+          }, $config.OTP_WILDCARD_DELAY);
+        }
       }
     }
   }
