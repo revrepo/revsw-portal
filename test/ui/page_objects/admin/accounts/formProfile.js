a16df4c008833338abe6c2131bb24f8ecd7f8004
AsherMoshav <asher@mediadome.co.il>
2017-09-20 10:38:44 +0300

    fixes for smoke tests

--- test/ui/page_objects/admin/accounts/formProfile.js
+++ test/ui/page_objects/admin/accounts/formProfile.js
@@ -54,7 +54,10 @@ var CompanyProfileForm = {
         id: 'address2'
       },
       country: {
-        id: 'Country'
+        id: 'Country',
+        inputField: {
+          css: '#Country .ui-select-search'
+        }
       },
       state: {
         id: 'state'
@@ -195,6 +198,18 @@ var CompanyProfileForm = {
    */
   getCountryDDown: function () {
     return element(by.id(this.locators.textInputs.country.id));
+  },
+
+  /**
+ * ### CompanyProfileForm.getCountryInput()
+ *
+ * Returns the reference to the `Country` input text field (Selenium WebDriver
+ * Element) from the Edit Form Company page from the Portal app.
+ *
+ * @returns {Selenium WebDriver Element}
+ */
+  getCountryInput: function () {
+    return element(by.css(this.locators.textInputs.country.inputField.css));
   },
 
   /**
@@ -369,9 +384,10 @@ var CompanyProfileForm = {
    * @returns {Promise}
    */
   setCountry: function (country) {
+    this.getCountryDDown().click(); // reveal input field
     return this
-      .getCountryDDown()
-      .sendKeys(country);
+      .getCountryInput()
+      .sendKeys(country + protractor.Key.ENTER);
   },
 
   /**
