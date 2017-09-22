e1434b27b304abaec1bdcdf4f0a0e46c41c215c1
AsherMoshav <asher@mediadome.co.il>
2017-09-22 21:01:58 +0300

    smoke config for jenkins

--- test/ui/config/smoke.js
+++ test/ui/config/smoke.js
@@ -5,7 +5,15 @@ smoke.capabilities = {
 };
 
 smoke.specs = [
-  '../suites/smoke/**/*Spec.js'
+  '../suites/workflow/sslCerts/addSSLCertSpec.js',
+  '../suites/workflow/domains/domainListSpec.js',
+  '../suites/functional/sideMenu/responsiveMenuSpec.js',
+  '../suites/functional/sideMenu/menuToggleBtnSpec.js',
+  '../suites/functional/companies/vendorChangeSpec.js',
+  '../suites/functional/help/helpSpec.js',
+  '../suites/functional/wafRules/wafRulesAccountLinkSpec.js',
+  '../suites/functional/whiteLabel/signUpPageFooterSpec.js',
+  '../suites/smoke/admin/accounts/editAccountSpec.js'
 ];
 
 exports.config = smoke;
