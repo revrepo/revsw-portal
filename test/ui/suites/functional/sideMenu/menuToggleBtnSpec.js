6cac20cb33e3ed382f2fc5efaabb0cd3fc26d0fb
AsherMoshav <asher@mediadome.co.il>
2017-09-22 15:36:34 +0300

    menu functional tests fixes

--- test/ui/suites/functional/sideMenu/menuToggleBtnSpec.js
+++ test/ui/suites/functional/sideMenu/menuToggleBtnSpec.js
@@ -25,7 +25,6 @@ describe('Functional', function () {
     describe('Side Menu Display Toggle Button', function () {
 
         var users = [
-            config.get('portal.users.admin'),
             config.get('portal.users.revAdmin'),
             config.get('portal.users.reseller')
         ];
@@ -48,6 +47,7 @@ describe('Functional', function () {
                 });
                 it('should show the side menu when clicked again', function () {
                     Portal.header.clickMenuToggleBtn();
+
                     expect(Portal.sideBar.getMenu().isDisplayed()).toBeTruthy();
                 });
             });
