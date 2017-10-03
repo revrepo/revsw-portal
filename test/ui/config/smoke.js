6ed131abf73bcfa9a4e6e6cafacca2f3c200269b
AsherMoshav <asher@mediadome.co.il>
2017-10-04 00:36:57 +0300

    config file smoke

--- test/ui/config/smoke.js
+++ test/ui/config/smoke.js
@@ -1,12 +1,11 @@
 var smoke = require('./base');
+
+smoke.capabilities = {
+  browserName: 'firefox'
+};
+
 smoke.specs = [
-  //'../suites/negative/2fa/twoFactorAuthSetupSpec.js',
-  //'../suites/negative/2fa/twoFactorAuthLoginSpec.js',
-  //'../suites/negative/domains/editDomainSpec.js',
-  '../suites/smoke/logShipping/addLogShippingSpec.js',
-  '../suites/smoke/wafRule/addWafRuleSpec.js',
-  '../suites/smoke/domains/addDomainSpec.js',
-  '../suites/smoke/accounts/accountBillingStatementsSpec.js'
+  '../suites/smoke/**/*Spec.js'
 ];
 
 exports.config = smoke;
