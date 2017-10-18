6d5f3e4ce6f78be69c84d1306f929de430ee6185
AsherMoshav <asher@mediadome.co.il>
2017-10-18 08:41:29 +0300

    technical debt little better (TODOs)

--- code/dev/js/portal/domains/origin-request-headers/originRequestHeaders.directive.js
+++ code/dev/js/portal/domains/origin-request-headers/originRequestHeaders.directive.js
@@ -1,5 +1,5 @@
 //origin-request-headers
-(function() {
+(function () {
   'use strict';
 
   angular.module('revapm.Portal.Domains')
@@ -32,7 +32,7 @@
          * @param  {Object} newOriginRequestHeader
          * @return
          */
-        this.onAddNew = function(newOriginRequestHeader) {
+        this.onAddNew = function (newOriginRequestHeader) {
           if (!_.isArray($ctrl.origin_request_headers)) {
             $ctrl.origin_request_headers = [];
           }
@@ -44,32 +44,31 @@
          * @param  {Integer} index
          * @return
          */
-        this.onDelete = function(index) {
-          // TODO: add confirm modal windows
+        this.onDelete = function (index) {
           var modalInstance = $uibModal.open({
             animation: true,
             templateUrl: 'parts/domains/modals/confirmDeleteHeaderInfo.tpl.html',
-            controller: /*ngInject*/ function($scope, $uibModalInstance, model) {
+            controller: /*ngInject*/ function ($scope, $uibModalInstance, model) {
               $scope.model = model;
-              $scope.ok = function() {
+              $scope.ok = function () {
                 $uibModalInstance.close('ok');
               };
-              $scope.cancel = function() {
+              $scope.cancel = function () {
                 $uibModalInstance.dismiss('cancel');
               };
             },
             // size: size,
             resolve: {
-              model: function() {
+              model: function () {
                 return $ctrl.origin_request_headers[index];
               }
             }
           });
 
           modalInstance.result
-            .then(function() {
+            .then(function () {
               $ctrl.origin_request_headers.splice(index, 1);
-            }, function(err) {
+            }, function (err) {
               // TODO: Alert ?
             });
 
