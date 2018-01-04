59d7fc8cf2aa7fedbd9a611c4db2ab7a517b8cf0
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2018-01-04 16:45:19 +0700

    Add UI check for auto-generation WAF Rule

--- code/dev/js/portal/waf_rules/controller/wafRulesAutoGenerateRulesModal.controller.js
+++ code/dev/js/portal/waf_rules/controller/wafRulesAutoGenerateRulesModal.controller.js
@@ -16,12 +16,8 @@
     $uibModal,
     $uibModalInstance,
     AlertService,
+    StatsWAF,
     model) {
-    $scope.newFilterState = {};
-
-    var defaultTimeToWaitNewNSONECallInMillissecons = $config.DNS_WAIT_NEW_CALL_MILLISSECONDS;
-
-    $scope.totalAddedRecords = 0;
     $scope.model = model;
     $scope._loading = false;
     $scope.domain = null;
@@ -79,6 +75,64 @@
                 deferred.reject(false);
               });
           }
+          return deferred.promise;
+        })
+        // NOTE: validation exists WAR rules in time period
+        .then(function() {
+          var _filters_field_list = ['from_timestamp', 'to_timestamp', 'report_type'];
+
+          function generateFilterParams(filters) {
+            var params = {
+              from_timestamp: moment().subtract(1, 'days').valueOf(),
+              to_timestamp: Date.now()
+            };
+            _.forEach(filters, function(val, key) {
+              if (_.indexOf(_filters_field_list, key) !== -1) {
+                if (val !== '-' && val !== '') {
+                  params[key] = val;
+                }
+              } else {
+                if (key === 'time_period') {
+                  var days = val.split(':');
+                  params.from_timestamp = !!days[0] ? moment(days[0]).valueOf() : moment().subtract(1, 'days').valueOf();
+                  params.to_timestamp = !!days[1] ? moment(days[1]).valueOf() : moment().valueOf();
+                  delete params.time_period;
+                }
+              }
+            });
+            return params;
+          }
+          var deferred = $q.defer();
+          // NOTE: use StatsWAF.topReport for detect empty data for auto-generation
+          var params = angular.merge({
+            domainId: $scope.model.domain.id,
+            report_type: 'rule_id'
+          }, generateFilterParams({
+            'time_period': $scope.model.time_period
+          }));
+          StatsWAF
+            .topReport(params)
+            .$promise
+            .then(function name(data) {
+              if (!data || !data.data || data.data.length === 0) {
+                // TODO: delete the alternative warning
+                // AlertService.danger('We don’t have any WAF security events detected for the domain for the requested period of time,' +
+                //   ' and as the result we cannot proceed with auto-generation of WAF rules.' +
+                //   ' Please check whether WAF is enabled for the domain and that the domain is receiving real end user traffic.');
+                $scope.confirm('parts/waf_rules/dialog/modal-war-rule-auto-generate-missing-data-warning.tpl.html', {
+                    text: 'We don’t have any WAF security events detected for the domain for the requested period of time,' +
+                      ' and as the result we cannot proceed with auto-generation of WAF rules.' +
+                      ' Please check whether WAF is enabled for the domain and that the domain is receiving real end user traffic.'
+                  })
+                  .then(function(data) {
+                    deferred.reject(false);
+                  })
+                  .catch(deferred.reject);
+              } else {
+                deferred.resolve(true);
+              }
+            })
+            .catch(deferred.reject);
           return deferred.promise;
         })
         .then(function(data) {
