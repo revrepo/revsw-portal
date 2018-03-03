15034234ba000a1070fefd978eab299bbf2db03c
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2018-03-04 00:08:10 +0700

    Add default text Domain Config versions

--- code/dev/js/portal/domains/controller/domainVersions.controller.js
+++ code/dev/js/portal/domains/controller/domainVersions.controller.js
@@ -49,7 +49,7 @@
 
       if (angular.isUndefined($scope.currentVersion) || $scope.currentVersion === null) {
         $scope.currentData = null;
-        $scope.obj.data = '';
+        $scope.obj.data = 'Configuration will appear here';
         return;
       }
       $scope._loading = true;
