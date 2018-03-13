e291a765441df8e6c6d33337aef396eb0558149a
AsherMoshav <asher@mediadome.co.il>
2018-03-13 12:53:45 +0200

    Updated groups UI templates

--- code/dev/js/portal/groups/controller/groups.controller.js
+++ code/dev/js/portal/groups/controller/groups.controller.js
@@ -8,7 +8,7 @@
   /*@ngInject*/
   function GroupsCrudController($scope, $q, CRUDController, Users, $rootScope,
     User, $injector, $state, $stateParams, Companies,
-    DomainsConfig, $anchorScroll, $config, Invitation, Groups) {
+    DomainsConfig, $anchorScroll, $config, Groups, Apps, DNSZones) {
 
     //Invoking crud actions
     $injector.invoke(CRUDController, this, {
@@ -20,10 +20,7 @@
       $state.go('index.accountSettings.profile');
       return;
     }
-    // NOTE: init load dependencies for Add New User Page
-    if ($state.current.name === 'index.accountSettings.groups.new') {
-      dependencies();
-    }
+
     //Set state (ui.router)
     $scope.setState('index.accountSettings.groups');
     $scope.setResource(Groups);
@@ -39,7 +36,60 @@
         $scope.model = {};
         angular.merge($scope.model, {
           name: '',
-          comment: ''
+          account_id: $scope.auth.isRevadmin() || $scope.auth.isReseller() ? '' : $scope.auth.getUser().companyId[0],
+          comment: '',
+          permissions: {
+            read_only: false,
+            enforce_2fa: false,
+            portal_login: true,
+            API_access: true,
+            dashboards: true,
+            mobile_apps: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            domains: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            ssl_names: true,
+            ssl_certs: true,
+            waf_rules: true,
+            cache_purge: true,
+            web_analytics: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            security_analytics: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            dns_zones: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            dns_analytics: true,
+            groups: true,
+            users: true,
+            API_keys: true,
+            logshipping_jobs: true,
+            activity_log: true,
+            accounts: {
+              access: true,
+              list: [],
+              allow_list: true
+            },
+            traffic_alerts: true,
+            notification_lists: true,
+            usage_reports: true,
+            billing_statements: true,
+            billing_plan: true
+          }
         });
       }
     }
@@ -51,13 +101,10 @@
 
     $scope.initNew = function (reinit) {
       initModel(reinit);
-      if ($scope.auth.isReseller() || $scope.auth.isRevadmin()) {
-        dependencies().then(function (data) {
-          $scope.setDefaultAccountId();
-        });
-      } else {
-        $scope.setDefaultAccountId();
-      }
+    };
+
+    $scope.setGroup = function (id) {
+      $scope.group_id = id;
     };
 
     $scope.getRelativeDate = function (datetime) {
@@ -76,12 +123,29 @@
           }
         };
       }
+
+      if ($state.is('index.accountSettings.groups.new') || $state.is('index.accountSettings.groups.edit')) {
+        Apps.query().$promise.then(function (apps) {
+          $scope.apps = apps;
+        });
+
+        DomainsConfig.query().$promise.then(function (domains) {
+          $scope.domains = domains;
+        });
+
+        DNSZones.query().$promise.then(function (zones) {
+          $scope.dnsZones = zones;
+        });
+      }
+
       $scope.list(data)
         .then(function (res) {
           // set our groups
           $scope.groups = res;
+
           // get companies and set the company names
           Companies.query().$promise.then(function (accs) {
+            $scope.companies = accs;
             $scope.groups.forEach(function (group) {
               accs.forEach(function (account) {
                 if (group.account_id === account.id) {
@@ -95,14 +159,60 @@
           Users.query().$promise.then(function (resultUsers) {
             $scope.groups.forEach(function (group) {
               resultUsers.forEach(function (usr) {
-                group.userCount = group.userCount || 0;
+                group.users = group.users || [];
                 if (group.id === usr.group_id) {
-                  group.userCount++;
+                  group.users.push(usr.email);
                 }
               });
             });
           });
         });
+    });
+
+    $scope.$on('$stateChangeSuccess', function (state) {
+      if ($state.is('index.accountSettings.groups.edit')) {
+        Groups.get({ id: $scope.group_id }).$promise.then(function (data) {
+          $scope.model = data;
+        });
+      }
+    });
+
+    $scope.pushItemsById = function (list, pushList, collection) {
+      $scope.model[pushList] = $scope.model[pushList] || [];
+      $scope.model.permissions[list].list.forEach(function (val) {
+        $scope[collection].forEach(function (fullVal) {
+          if (fullVal.id === val) {
+
+            $scope.model[pushList].push(fullVal);
+          }
+        });
+      });
+    };
+
+    $scope.$watch('apps', function () {
+      if ($scope.apps) {
+        $scope.pushItemsById('mobile_apps', 'apps_list', 'apps');
+      }
+    });
+
+    $scope.$watch('domains', function () {
+      if ($scope.domains) {
+        $scope.pushItemsById('domains', 'domains_list', 'domains');
+        $scope.pushItemsById('web_analytics', 'web_analytics_list', 'domains');
+        $scope.pushItemsById('security_analytics', 'security_analytics_list', 'domains');
+      }
+    });
+
+    $scope.$watch('dnsZones', function () {
+      if ($scope.dnsZones) {
+        $scope.pushItemsById('dns_zones', 'dns_zones_list', 'dnsZones');
+      }
+    });
+
+    $scope.$watch('companies', function () {
+      if ($scope.companies) {
+        $scope.pushItemsById('accounts', 'accounts_list', 'companies');
+      }
     });
 
     /**
@@ -125,6 +235,179 @@
             .then($scope.alertService.success)
             .catch($scope.alertService.danger);
         });
+    };
+
+    /**
+     * @name  addItemToList
+     * @description
+     *
+     * Add an item to a list of items (apps, domains, dns_zones...)
+     *
+     * @param  {[type]} item [description]
+     * @return {[type]}       [description]
+     */
+    $scope.addItemToList = function (item, list) {
+      if (item && list) {
+        if (!$scope.model.permissions[list]) {
+          $scope.model.permissions[list] = {
+            access: true,
+            list: [],
+            allow_list: true
+          };
+        }
+
+        if (!$scope.model.permissions[list].list || !$scope.model.permissions[list].list.length) {
+          $scope.model.permissions[list].list = [];
+        }
+
+        if ($scope.model.permissions[list].list.indexOf(item.id) === -1) {
+          $scope.model.permissions[list].list.push(item.id);
+        }
+      }
+    };
+
+    /**
+     * @name  removeItemFromList
+     * @description
+     *
+     * Remove an item from a list of items (apps, domains, dns_zones...)
+     *
+     * @param  {[type]} item [description]
+     * @return {[type]}       [description]
+     */
+    $scope.removeItemFromList = function (item, list) {
+      return $scope
+        .model
+        .permissions[list]
+        .list
+        .splice($scope.model.permissions[list].list.indexOf(item.id), 1);
+    };
+
+    $scope.createGroup = function (model) {
+      if (!model) {
+        return;
+      }
+
+      delete model.apps_list;
+      delete model.domains_list;
+      delete model.dns_zones_list;
+      delete model.security_analytics_list;
+      delete model.web_analytics_list;
+      delete model.accounts_list;
+
+
+      $scope.create(model)
+        .then(function (data) {
+          initModel(true);
+          $scope.clearModel();
+          $scope.alertService.success(data);
+        })
+        .catch($scope.alertService.danger);
+    };
+
+    $scope.updateGroup = function (model) {
+      if (!model) {
+        return;
+      }
+
+      delete model.apps_list;
+      delete model.domains_list;
+      delete model.dns_zones_list;
+      delete model.security_analytics_list;
+      delete model.web_analytics_list;
+      delete model.accounts_list;
+
+      $scope.update(model)
+        .then(function (data) {
+          initModel(true);
+          $scope.clearModel();
+          $scope.alertService.success(data);
+          $state.go('^');
+        })
+        .catch($scope.alertService.danger);
+    };
+
+    $scope.getMobileAppName = function (id) {
+      if (!$scope.apps) {
+        return;
+      }
+      var appToReturn;
+      $scope.apps.forEach(function (app) {
+        if (app.id === id) {
+          appToReturn = app;
+        }
+      });
+      return appToReturn;
+    };
+
+    $scope.getDomain = function (id) {
+      if (!$scope.domains) {
+        return;
+      }
+      var domainToReturn;
+      $scope.domains.forEach(function (domain) {
+        if (domain.id === id) {
+          domainToReturn = domain;
+        }
+      });
+      return domainToReturn;
+    };
+
+    $scope.getDNSZone = function (id) {
+      if (!$scope.dnsZones) {
+        return;
+      }
+      var zoneToReturn;
+      $scope.dnsZones.forEach(function (zone) {
+        if (zone.id === id) {
+          zoneToReturn = zone;
+        }
+      });
+      return zoneToReturn;
+    };
+
+    $scope.getAccount = function (id) {
+      if (!$scope.companies) {
+        return;
+      }
+      var accR;
+      $scope.companies.forEach(function (acc) {
+        if (acc.id === id) {
+          accR = acc;
+        }
+      });
+      return accR;
+    };
+
+    $scope.toggleAllowList = function (list) {
+      if (!$scope.model.permissions[list]) {
+        $scope.model.permissions[list] = {
+          access: true,
+          list: [],
+          allow_list: false
+        };
+      }
+
+      if (!$scope.model.permissions[list].list || !$scope.model.permissions[list].list.length) {
+        $scope.model.permissions[list].list = [];
+      }
+
+      $scope.model.permissions[list].allow_list = !$scope.model.permissions[list].allow_list;
+      $scope.getAllowDenyStatus(list);
+    };
+
+    $scope.getAllowDenyStatus = function (list) {
+      if ($scope.model.permissions[list]) {
+        return $scope.model.permissions[list].allow_list ? 'Deny' : 'Allow';
+      }
+      return;
+    };
+
+    $scope.disableSubmit = function (model) {
+      return !model.account_id ||
+        model.account_id === '' ||
+        !model.name ||
+        model.name === '';
     };
   }
 })();
